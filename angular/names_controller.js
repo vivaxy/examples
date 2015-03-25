761f8df2bc1cf5b24cb3a2ff45f177021f7d0c2c
vivaxy <xyxuye2007@126.com>
2015-03-25 21:31:50 +0800

    add

---
+++ angular/names_controller.js
@@ -0,0 +1,11 @@
+/**
+ * @since 2014/10/8 8:52
+ * @author vivaxy
+ */
+function namesController($scope) {
+    $scope.names = [
+        {name: 'Jani', country: 'Norway'},
+        {name: 'Hege', country: 'Sweden'},
+        {name: 'Kai', country: 'Denmark'}
+    ];
+}
