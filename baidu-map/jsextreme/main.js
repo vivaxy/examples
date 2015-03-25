850a13a7d28f88c0a3f4718ef534d22eca8571c0
vivaxy <xyxuye2007@126.com>
2015-03-25 21:39:38 +0800

    add

---
+++ baidu-map/jsextreme/main.js
@@ -0,0 +1,42 @@
+/**
+ * @since 150116 10:16
+ * @author vivaxy
+ */
+// 创建地图实例
+var map = new BMap.Map("container");
+// 上海
+var point = new BMap.Point(121.426907, 31.221611);
+// 北京
+var point1 = new BMap.Point(116.404, 39.915);
+// 初始化地图，设置中心点坐标和地图级别
+map.centerAndZoom(point1, 15);
+
+// 2秒后
+setTimeout(function () {
+  //移动到公司
+  map.panTo(point);
+  //1秒后
+  setTimeout(function () {
+    //缩放
+    map.setZoom(30);
+  }, 1000)
+}, 2000);
+
+//地理位置解析
+var geoc = new BMap.Geocoder();
+
+//点击
+map.addEventListener("click", function (e) {
+  console.log('clicked ', e.point.lng, e.point.lat);
+  geoc.getLocation(e.point, function (rs) {
+    var addComp = rs.addressComponents;
+    console.log(addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber);
+  });
+});
+
+//拖动事件
+map.addEventListener("dragend", function () {
+  var center = map.getCenter();
+  console.log('center moved to ', center.lng, center.lat);
+});
+
