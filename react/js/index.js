1595adee1abfb907ae1bfadc03b546e8130b7ce7
vivaxy <xyxuye2007@126.com>
2015-04-03 15:44:30 +0800

    index

--- react/js/index.js
+++ react/js/index.js
@@ -1,13 +1,46 @@
-var CommentBox = React.createClass({displayName: "CommentBox",
-    render: function() {
+var Example = React.createClass({displayName: "Example",
+    render: function () {
+        return (
+            React.createElement("a", {href: this.props.data.link}, this.props.data.name)
+        );
+    }
+});
+
+var ExampleList = React.createClass({displayName: "ExampleList",
+    componentDidMount: function () {
+        var _this = this;
+        new Ajax({
+            url: './data/examples.json'
+        }).on('success', function (data) {
+            _this.setState({
+                data: JSON.parse(data)
+            });
+        }).send();
+    },
+    getInitialState: function () {
+        // initial state
+        return {
+            data: [{
+                link: '#',
+                name: 'loading'
+            }]
+        };
+    },
+    render: function () {
+        var list = this.state.data.map(function (a) {
+            return (
+                React.createElement("li", null, React.createElement(Example, {data: a}))
+            );
+        });
         return (
             React.createElement("ul", null, 
-                "Hello, world!"
+                list
             )
         );
     }
 });
+
 React.render(
-    React.createElement(CommentBox, null),
+    React.createElement(ExampleList, null),
     document.querySelector('.content')
 );
--- react/jsx/index.jsx
+++ react/jsx/index.jsx
@@ -1,13 +1,46 @@
-var CommentBox = React.createClass({
-    render: function() {
+var Example = React.createClass({
+    render: function () {
+        return (
+            <a href={this.props.data.link}>{this.props.data.name}</a>
+        );
+    }
+});
+
+var ExampleList = React.createClass({
+    componentDidMount: function () {
+        var _this = this;
+        new Ajax({
+            url: './data/examples.json'
+        }).on('success', function (data) {
+            _this.setState({
+                data: JSON.parse(data)
+            });
+        }).send();
+    },
+    getInitialState: function () {
+        // initial state
+        return {
+            data: [{
+                link: '#',
+                name: 'loading'
+            }]
+        };
+    },
+    render: function () {
+        var list = this.state.data.map(function (a) {
+            return (
+                <li><Example data={a} /></li>
+            );
+        });
         return (
             <ul>
-                Hello, world!
+                {list}
             </ul>
         );
     }
 });
+
 React.render(
-    <CommentBox />,
+    <ExampleList />,
     document.querySelector('.content')
 );
