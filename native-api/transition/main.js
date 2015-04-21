a2766d179f0bb964aa70f83358bdd2c2a64f8bc1
vivaxy <xyxuye2007@126.com>
2015-04-21 12:03:29 +0800

    getTransitionEndEventName

--- native-api/transition/main.js
+++ native-api/transition/main.js
@@ -2,12 +2,27 @@
  * @since 150119 09:43
  * @author vivaxy
  */
-var demo = document.querySelector('.demo');
-demo.addEventListener('webkitTransitionEnd', function(e){
-  console.log('webkitTransitionEnd', e);
-  demo.classList.toggle('to');
+var demo = document.querySelector('.demo'),
+    getTransitionEndEventName = function () {
+        var transitions = {
+                "transition": "transitionend",
+                "WebkitTransition": "webkitTransitionEnd",
+                "MozTransition": "transitionend"
+            },
+            el = document.createElement("div");
+        for (var i in transitions) {
+            if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+                return transitions[i];
+            }
+        }
+        return 'transitionend';
+    };
+
+demo.addEventListener(getTransitionEndEventName(), function (e) {
+    console.log('transitionEnd', e);
+    demo.classList.toggle('to');
 }, false);
-//demo.addEventListener('transitionEnd', function(e){
-//  console.log('transitionEnd', e);
-//}, false);
-demo.classList.add('to');
+
+setTimeout(function () {
+    demo.classList.add('to');
+}, 0);
