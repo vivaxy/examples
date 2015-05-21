2470a2eb2b8cfed75c91a08ea312fa25abd70a87
vivaxy <xyxuye2007@126.com>
2015-05-21 20:26:36 +0800

    ECMAScript-6

--- javascript/console/index.js
+++ javascript/console/index.js
@@ -2,14 +2,7 @@
  * @since 150521 09:54
  * @author vivaxy
  */
+var button = document.createElement('div');
 
-var button = 0;
+document.body.appendChild(button);
 
-(function () {
-    var button = 1;
-    //document.body.appendChild('');
-    console.log(button);
-
-})();
-
-console.log(button);
