3496585ebc31c14f0f19b7a680a94f52409b6ca4
vivaxy <xyxuye2007@126.com>
2015-05-21 18:32:38 +0800

    template

---
+++ javascript/console/index.js
@@ -0,0 +1,15 @@
+/**
+ * @since 150521 09:54
+ * @author vivaxy
+ */
+
+var button = 0;
+
+(function () {
+    var button = 1;
+    //document.body.appendChild('');
+    console.log(button);
+
+})();
+
+console.log(button);
--- javascript/template-engine/index.js
+++ javascript/template-engine/index.js
@@ -4,37 +4,22 @@
  */
 /**
  * test 1:
- * global variables
+ * level 1 variables
  */
-var toHtml1 = compile(function () {/*
- <div>
-     <h2>${title}</h2>
-     <div class="content">${content}</div>
- </div>
- */
-});
-var title = 'title string 1',
-    content = 'content string 1';
-document.body.innerHTML += toHtml1(window);
+var test2 = {
+    title: 'title string 1',
+    content: 'content string 1'
+};
+document.body.innerHTML += toHtml1(test2);
 
 /**
  * test 2:
  * level 2 variables
  */
-var test2 = {
-    title: 'title string 2',
-    content: 'content string 2'
-};
-document.body.innerHTML += toHtml1(test2);
-
-/**
- * test 3:
- * level 3 variables
- */
 var test3 = {
     data: {
-        title: 'title string 3',
-        content: 'content string 3'
+        title: 'title string 2',
+        content: 'content string 2'
     }
 };
 var toHtml3 = compile(function () {/*
