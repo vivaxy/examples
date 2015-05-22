11e86c2f629768fd55d6b03f519d08c737990f1f
vivaxy <xyxuye2007@126.com>
2015-05-22 09:51:25 +0800

    fix-template-engine

--- javascript/template-engine/index.js
+++ javascript/template-engine/index.js
@@ -6,27 +6,33 @@
  * test 1:
  * level 1 variables
  */
-var test2 = {
+var toHtml1 = compile(function () {/*
+ <div>
+     <h2>${title}</h2>
+     <div class="content">${content}</div>
+ </div>    
+*/});
+var test1 = {
     title: 'title string 1',
     content: 'content string 1'
 };
-document.body.innerHTML += toHtml1(test2);
+document.body.innerHTML += toHtml1(test1);
 
 /**
  * test 2:
  * level 2 variables
  */
-var test3 = {
+var test2 = {
     data: {
         title: 'title string 2',
         content: 'content string 2'
     }
 };
-var toHtml3 = compile(function () {/*
+var toHtml2 = compile(function () {/*
  <div>
      <h2>${data.title}</h2>
      <div class="content">${data.content}</div>
  </div>
  */
 });
-document.body.innerHTML += toHtml3(test3);
+document.body.innerHTML += toHtml2(test2);
