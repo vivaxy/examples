254dea4cd07ccc343ea8dc52a065133d4a950d04
vivaxy <xyxuye2007@126.com>
2015-05-31 13:59:09 +0800

    angular-material

---
+++ angular-material/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * @since 150531 13:32
+ * @author vivaxy
+ */
+angular.module('menu', ['ngMaterial'])
+    .controller('list', function ($scope) {
+        $scope.list = ['name', 'another name'];
+    });
