d86a52b475f576ad8b113b5fecf24b6f848abc4c
vivaxy <xyxuye2007@126.com>
2015-08-14 13:30:57 +0800

    event-delegation-on-ios-safari

---
+++ pitfall/event-delegation-on-ios-safari/index.js
@@ -0,0 +1,7 @@
+/**
+ * @since 15-08-03 11:08
+ * @author vivaxy
+ */
+$('body').on('click', '.J_button', function (e) {
+    alert('clicked');
+});
--- pitfall/event-delegation-on-safari/index.js
+++
@@ -1,4 +0,0 @@
-/**
- * @since 15-08-03 11:08
- * @author vivaxy
- */
