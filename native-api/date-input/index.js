aeb78a23284d946a68f480fa9c4006cd35cab6e4
vivaxy <xyxuye2007@126.com>
2015-09-18 12:10:05 +0800

    add date-input

---
+++ native-api/date-input/index.js
@@ -0,0 +1,9 @@
+/**
+ * @since 2015-09-17 15:14
+ * @author vivaxy
+ */
+var input = document.querySelector('input');
+
+input.addEventListener('change', function () {
+    console.log('date picked:', input.value);
+}, false);
