ed7e8f1135f0e57e65cf72304a6c4e278acd107e
vivaxy <xyxuye2007@126.com>
2016-08-17 10:12:22 +0800

    update is element on screen

--- javascript/is-element-on-screen/index.js
+++ javascript/is-element-on-screen/index.js
@@ -5,47 +5,47 @@
 
 'use strict';
 
-//        var IN_SCREEN_HEIGHT = 0;
-var IN_SCREEN_HEIGHT = 50;
-//    var IN_SCREEN_HEIGHT = Infinity;
-//        var IN_SCREEN_WIDTH = 0;
-var IN_SCREEN_WIDTH = 50;
-//    var IN_SCREEN_WIDTH = Infinity;
+// var ON_SCREEN_HEIGHT = 0;
+var ON_SCREEN_HEIGHT = 50;
+// var ON_SCREEN_HEIGHT = Infinity;
+// var ON_SCREEN_WIDTH = 0;
+var ON_SCREEN_WIDTH = 50;
+// var ON_SCREEN_WIDTH = Infinity;
 
-var isInScreen = function (element) {
+var isOnScreen = function (element) {
 
     var rect = element.getBoundingClientRect();
-    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth;
 
     var elementHeight = element.offsetHeight;
     var elementWidth = element.offsetWidth;
 
-    var inScreenHeight = IN_SCREEN_HEIGHT > elementHeight ? elementHeight : IN_SCREEN_HEIGHT;
-    var inScreenWidth = IN_SCREEN_WIDTH > elementWidth ? elementWidth : IN_SCREEN_WIDTH;
+    var onScreenHeight = ON_SCREEN_HEIGHT > elementHeight ? elementHeight : ON_SCREEN_HEIGHT;
+    var onScreenWidth = ON_SCREEN_WIDTH > elementWidth ? elementWidth : ON_SCREEN_WIDTH;
 
     // 元素在屏幕上方
     var elementBottomToWindowTop = rect.top + elementHeight;
-    var bottomBorderInScreen = elementBottomToWindowTop >= inScreenHeight;
+    var bottomBoundingOnScreen = elementBottomToWindowTop >= onScreenHeight;
 
     // 元素在屏幕下方
     var elementTopToWindowBottom = windowHeight - (rect.bottom - elementHeight);
-    var topBorderInScreen = elementTopToWindowBottom >= inScreenHeight;
+    var topBoundingOnScreen = elementTopToWindowBottom >= onScreenHeight;
 
     // 元素在屏幕左侧
     var elementRightToWindowLeft = rect.left + elementWidth;
-    var rightBorderInScreen = elementRightToWindowLeft >= inScreenWidth;
+    var rightBoundingOnScreen = elementRightToWindowLeft >= onScreenWidth;
 
     // 元素在屏幕右侧
     var elementLeftToWindowRight = windowWidth - (rect.right - elementWidth);
-    var leftBorderInScreen = elementLeftToWindowRight >= inScreenWidth;
+    var leftBoundingOnScreen = elementLeftToWindowRight >= onScreenWidth;
 
-    return bottomBorderInScreen && topBorderInScreen && rightBorderInScreen && leftBorderInScreen;
+    return bottomBoundingOnScreen && topBoundingOnScreen && rightBoundingOnScreen && leftBoundingOnScreen;
 };
 
 var element = document.querySelector('.js-element');
 
 setInterval(function () {
-    var inScreen = isInScreen(element);
-    console.log(inScreen);
+    var onScreen = isOnScreen(element);
+    console.log(onScreen);
 }, 50);
