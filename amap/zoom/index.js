f2cbdc00a6eba2fc9a4760f2347fb2e09b5109dc
vivaxy <xyxuye2007@126.com>
2016-10-11 16:00:58 +0800

    add amap zoom

--- amap/zoom/index.js
+++ amap/zoom/index.js
@@ -9,7 +9,7 @@ var zoomOut = document.querySelector('.js-zoom-out');
 var maxZoom = 19;
 var minZoom = 3;
 
-var zoom = 19;
+var zoom = 14;
 var center = [
     121.4203236,
     31.2162311
@@ -26,30 +26,12 @@ var map = new AMap.Map('container', {
     animateEnable: true,
 });
 
-var zoomToRadius = () => {
-    // radius in meter
-    var radius = map.getBounds().getCenter().distance(map.getBounds().getSouthWest());
-    return radius / 2;
-};
-
-var circle = new AMap.Circle({
-    center: new AMap.LngLat(121.4203236, 31.2162311), // 圆心位置
-    radius: zoomToRadius(), //半径
-    strokeColor: '#f33', //线颜色
-    strokeOpacity: 1, //线透明度
-    strokeWeight: 1, //线粗细度
-    fillColor: '#ee2200', //填充颜色
-    fillOpacity: 0.2 //填充透明度
-});
-circle.setMap(map);
-
 zoomIn.addEventListener('click', () => {
     // map.setZoomAndCenter(zoom++, center);
     if (zoom < maxZoom) {
         zoom++;
         map.setZoom(zoom);
         zoomOut.removeAttribute('disabled');
-        circle.setRadius(zoomToRadius());
     } else {
         zoomIn.setAttribute('disabled', true);
     }
@@ -61,7 +43,6 @@ zoomOut.addEventListener('click', () => {
         zoom--;
         map.setZoom(zoom);
         zoomIn.removeAttribute('disabled');
-        circle.setRadius(zoomToRadius());
     } else {
         zoomOut.setAttribute('disabled', true);
     }
