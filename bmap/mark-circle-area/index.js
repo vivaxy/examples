7169c96301382630b4effb033cc9fb935fa2545d
vivaxy <xyxuye2007@126.com>
2016-10-11 20:33:44 +0800

    add zoom

--- bmap/mark-circle-area/index.js
+++ bmap/mark-circle-area/index.js
@@ -8,10 +8,11 @@ var lng = 121.4203236;
 var lat = 31.2162311;
 var radius = 1000;
 
+var container = document.querySelector('.js-container');
 var centerPoint = new BMap.Point(lng, lat);
 
 // 百度地图API功能
-var map = new BMap.Map('allmap');    // 创建Map实例
+var map = new BMap.Map(container);    // 创建Map实例
 map.centerAndZoom(centerPoint, zoom);  // 初始化地图,设置中心点坐标和地图级别
 map.disableDragging();
 map.disableDoubleClickZoom();
--- bmap/mark-polygon-area/index.js
+++ bmap/mark-polygon-area/index.js
@@ -7,10 +7,11 @@ var zoom = 14;
 var lng = 121.4203236;
 var lat = 31.2162311;
 
+var container = document.querySelector('.js-container');
 var centerPoint = new BMap.Point(lng, lat);
 
 // 百度地图API功能
-var map = new BMap.Map('allmap');    // 创建Map实例
+var map = new BMap.Map(container);    // 创建Map实例
 map.centerAndZoom(centerPoint, zoom);  // 初始化地图,设置中心点坐标和地图级别
 map.disableDragging();
 map.disableDoubleClickZoom();
