8903d24298d384368aeb2e500495d6fe7a236fa3
vivaxy <xyxuye2007@126.com>
2017-03-06 18:30:19 +0800

    feat(babel-plugin-import): :sparkles:

---
+++ babel-plugin-import/index.babel.js
@@ -0,0 +1,8 @@
+/**
+ * @since 2017-03-06 17:58
+ * @author vivaxy
+ */
+
+import { Button, } from 'antd';
+
+export default <Button>test</Button>;
---
+++ babel-plugin-import/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _css = require('antd/lib/button/style/css');
+
+var _button = require('antd/lib/button');
+
+var _button2 = _interopRequireDefault(_button);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = React.createElement(
+  _button2.default,
+  null,
+  'test'
+); /**
+    * @since 2017-03-06 17:58
+    * @author vivaxy
+    */
