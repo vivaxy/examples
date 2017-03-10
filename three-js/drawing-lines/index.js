80b7b416086ea0e1d8a51689ddf9d48c69a20420
vivaxy <xyxuye2007@126.com>
2017-03-10 17:45:15 +0800

    feat(three-js): :sparkles:drawing-lines

---
+++ three-js/drawing-lines/index.js
@@ -0,0 +1,24 @@
+/**
+ * @since 2017-03-10 17:31
+ * @author vivaxy
+ */
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+var scene = new THREE.Scene();
+
+// create a blue LineBasicMaterial
+var material = new THREE.LineBasicMaterial({color: 0x0000ff});
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+var line = new THREE.Line(geometry, material);
+scene.add(line);
+renderer.render(scene, camera);
