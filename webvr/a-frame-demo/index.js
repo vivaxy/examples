fb2d5d03c06bb09a3125236d04146ea33be29b3a
vivaxy <xyxuye2007@126.com>
2017-03-19 11:53:21 +0800

    feat(webvr): :sparkles:a-frame-minecraft

--- webvr/a-frame-demo/index.js
+++ webvr/a-frame-demo/index.js
@@ -3,3 +3,17 @@
  * @author vivaxy
  * @see https://css-tricks.com/minecraft-webvr-html-using-frame/
  */
+
+// Query scene graph using `querySelector`.
+var sceneEl = document.querySelector('a-scene');
+var boxEl = sceneEl.querySelector('a-box');
+
+// Get data about entity with `getAttribute`.
+console.log(boxEl.getAttribute('position'));
+// >> {x: -1, y: 0.5, z: -3}
+
+// Add event listener with `addEventListener`.
+boxEl.addEventListener('click', function() {
+    // Modify entity with `setAttribute`.
+    boxEl.setAttribute('color', 'red');
+});
