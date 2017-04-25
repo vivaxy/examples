fdb062813636a1c18d145123c096bfc038986951
vivaxy <xyxuye2007@126.com>
2017-04-25 17:44:48 +0800

    feat(react-native): :sparkles:lottie_react_native

---
+++ react-native/lottie_react_native/src/index.js
@@ -0,0 +1,41 @@
+/**
+ * @since 2017-04-25 15:27:23
+ * @author vivaxy
+ */
+
+import React, { Component } from 'react';
+import Animation from 'lottie-react-native';
+import { StyleSheet } from 'react-native';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.setAnimationRef = this.setAnimationRef.bind(this);
+    }
+
+    componentDidMount() {
+        this.animation.play();
+    }
+
+    setAnimationRef(ref) {
+        this.animation = ref;
+    }
+
+    render() {
+        return (
+            <Animation
+                loop
+                ref={this.setAnimationRef}
+                style={styles.animation}
+                source={require('./navidad.json')}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    animation: {
+        flex: 1,
+    },
+});
