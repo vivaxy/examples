66f9d501bb0a66125faa11de1e5e0b56af1f72d4
vivaxy <xyxuye2007@126.com>
2017-05-07 13:04:57 +0800

    feat(native-api): :sparkles:animate

---
+++ native-api/animate/index.js
@@ -0,0 +1,24 @@
+/**
+ * @since 2017-05-07 11:51:05
+ * @author vivaxy
+ */
+
+const elemenet = document.querySelector('.js-element');
+
+const growKeyframes = [
+    { transform: 'none' },
+    { transform: 'scale(2)' },
+];
+
+const alteredGrowOptions = {
+    duration: 3000,
+    iterations: Infinity,
+    direction: 'alternate',
+    delay: 0,
+};
+
+const myAnimation = elemenet.animate(growKeyframes, alteredGrowOptions);
+
+//
+myAnimation.pause();
+myAnimation.play();
