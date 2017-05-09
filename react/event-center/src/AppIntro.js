99cb2769d74f67f30fc0130357ec0cc4de21e71a
vivaxy <xyxuye2007@126.com>
2017-05-09 10:56:53 +0800

    feat(react): :sparkles:event-center

---
+++ react/event-center/src/AppIntro.js
@@ -0,0 +1,39 @@
+/**
+ * @since 2017-05-09 10:20:57
+ * @author vivaxy
+ */
+
+import React, { Component } from 'react';
+
+export default class AppIntro extends Component {
+
+    state = {
+        count: 1,
+    };
+
+    componentDidMount() {
+        const { events } = this.props;
+        events.on('add', (value) => {
+            this.setState({
+                count: this.state.count + value,
+            });
+        });
+    }
+
+    render() {
+        const { events } = this.props;
+        const { count } = this.state;
+        return (
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                <p>{count}</p>
+                <p onClick={() => {
+                    events.emit('minus', 1);
+                    this.setState({
+                        count: this.state.count - 1,
+                    });
+                }}>minus</p>
+            </p>
+        );
+    }
+}
---
+++ react/event-center/src/events.js
@@ -0,0 +1,8 @@
+/**
+ * @since 2017-05-09 10:50:57
+ * @author vivaxy
+ */
+
+import EventEmitter from 'eventemitter3';
+
+export default new EventEmitter();
