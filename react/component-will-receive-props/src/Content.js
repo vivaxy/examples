c684d2fbbc48eb1080ab246a2a066e31611df862
vivaxy <xyxuye2007@126.com>
2017-05-11 18:44:51 +0800

    feat(react): :sparkles:component-will-receive-props

---
+++ react/component-will-receive-props/src/Content.js
@@ -0,0 +1,27 @@
+/**
+ * @since 2017-05-11 17:57:28
+ * @author vivaxy
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Content extends Component {
+
+    static contextTypes = {
+        store: PropTypes.object,
+    };
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log('Content componentWillReceiveProps', nextProps, nextContext);
+    }
+
+    render() {
+        console.log('Content render');
+        return (
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        );
+    }
+}
