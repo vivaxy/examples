34a1eb5126e2c3fca3ca289aafdcfecd83e21a0f
vivaxy <xyxuye2007@126.com>
2017-11-26 17:12:28 +0800

    chore:

--- native-api/webrtc/peer-connection/index.js
+++ native-api/webrtc/peer-connection/index.js
@@ -3,6 +3,7 @@ const pcList = [];
 const pc = new RTCPeerConnection();
 
 pc.onicecandidate = (event) => {
+    console.log(event);
     if (event.candidate) {
         console.log('The ICE candidate (transport address: ' + event.candidate.candidate + ') has been added to this connection.');
         // Send the candidate to the remote peer
