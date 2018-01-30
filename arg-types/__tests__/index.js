3f34a80b61040274e45a33010f7afcf1e8f94e44
vivaxy <xyxuye2007@126.com>
2018-01-30 18:19:56 +0800

    feat(ArgTypes): :sparkles:Add merge method

--- arg-types/__tests__/index.js
+++ arg-types/__tests__/index.js
@@ -4,49 +4,49 @@
  */
 
 const test = require('ava');
-const PropTypes = require('..');
+const ArgTypes = require('..');
 
 class Message {
 
 }
 
-const propTypes = {
-    string: PropTypes.string,
-    requiredString: PropTypes.string.isRequired,
-    bool: PropTypes.bool,
-    requiredBool: PropTypes.bool.isRequired,
-    number: PropTypes.number,
-    requiredNumber: PropTypes.number.isRequired,
-    symbol: PropTypes.symbol,
-    requiredSymbol: PropTypes.symbol.isRequired,
-    func: PropTypes.func,
-    requiredFunc: PropTypes.func.isRequired,
-    object: PropTypes.object,
-    requiredObject: PropTypes.object.isRequired,
-    array: PropTypes.array,
-    requiredArray: PropTypes.array.isRequired,
-    shape: PropTypes.shape({}),
-    requiredShape: PropTypes.shape({
-        requiredNumber: PropTypes.number.isRequired,
+const argTypes = {
+    string: ArgTypes.string,
+    requiredString: ArgTypes.string.isRequired,
+    bool: ArgTypes.bool,
+    requiredBool: ArgTypes.bool.isRequired,
+    number: ArgTypes.number,
+    requiredNumber: ArgTypes.number.isRequired,
+    symbol: ArgTypes.symbol,
+    requiredSymbol: ArgTypes.symbol.isRequired,
+    func: ArgTypes.func,
+    requiredFunc: ArgTypes.func.isRequired,
+    object: ArgTypes.object,
+    requiredObject: ArgTypes.object.isRequired,
+    array: ArgTypes.array,
+    requiredArray: ArgTypes.array.isRequired,
+    shape: ArgTypes.shape({}),
+    requiredShape: ArgTypes.shape({
+        requiredNumber: ArgTypes.number.isRequired,
     }).isRequired,
-    instanceOf: PropTypes.instanceOf(Message),
-    requiredInstanceOf: PropTypes.instanceOf(Message).isRequired,
-    oneOf: PropTypes.oneOf([1, 2]),
-    requiredOneOf: PropTypes.oneOf([1, 2]).isRequired,
-    oneOfType: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
+    instanceOf: ArgTypes.instanceOf(Message),
+    requiredInstanceOf: ArgTypes.instanceOf(Message).isRequired,
+    oneOf: ArgTypes.oneOf([1, 2]),
+    requiredOneOf: ArgTypes.oneOf([1, 2]).isRequired,
+    oneOfType: ArgTypes.oneOfType([
+        ArgTypes.string,
+        ArgTypes.number,
     ]),
-    requiredOneOfType: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
+    requiredOneOfType: ArgTypes.oneOfType([
+        ArgTypes.string,
+        ArgTypes.number,
     ]).isRequired,
-    arrayOfType: PropTypes.arrayOfType(PropTypes.number),
-    requiredArrayOfType: PropTypes.arrayOfType(PropTypes.number).isRequired,
-    any: PropTypes.any,
-    requiredAny: PropTypes.any.isRequired,
-    custom: (props, propName) => {
-        if (props[propName] !== 'custom') {
+    arrayOfType: ArgTypes.arrayOfType(ArgTypes.number),
+    requiredArrayOfType: ArgTypes.arrayOfType(ArgTypes.number).isRequired,
+    any: ArgTypes.any,
+    requiredAny: ArgTypes.any.isRequired,
+    custom: (args, argName) => {
+        if (args[argName] !== 'custom') {
             throw new Error('Custom Error');
         }
     },
@@ -89,344 +89,356 @@ const object = {
 
 test('pass', (t) => {
     t.notThrows(() => {
-        PropTypes.check(propTypes, object);
+        ArgTypes.check(argTypes, object);
     });
 });
 
 test('pass on optional string, not provided', (t) => {
     t.notThrows(() => {
         const { string, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional string, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, string: 1 });
+        ArgTypes.check(argTypes, { ...object, string: 1 });
     }, Error);
 });
 
 test('throw on required string, not provided', (t) => {
     t.throws(() => {
         const { requiredString, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required string, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredString: 1 });
+        ArgTypes.check(argTypes, { ...object, requiredString: 1 });
     }, Error);
 });
 
 test('pass on optional boolean, not provided', (t) => {
     t.notThrows(() => {
         const { bool, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional bool, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, bool: 1 });
+        ArgTypes.check(argTypes, { ...object, bool: 1 });
     }, Error);
 });
 
 test('throw on required boolean, not provided', (t) => {
     t.throws(() => {
         const { requiredBool, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required boolean, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredBool: 1 });
+        ArgTypes.check(argTypes, { ...object, requiredBool: 1 });
     }, Error);
 });
 
 test('pass on optional number, not provided', (t) => {
     t.notThrows(() => {
         const { number, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional number, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, number: '1' });
+        ArgTypes.check(argTypes, { ...object, number: '1' });
     }, Error);
 });
 
 test('throw on required number, not provided', (t) => {
     t.throws(() => {
         const { requiredNumber, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required number, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredNumber: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredNumber: '1' });
     }, Error);
 });
 
 test('pass on optional symbol, not provided', (t) => {
     t.notThrows(() => {
         const { symbol, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional symbol, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, symbol: '1' });
+        ArgTypes.check(argTypes, { ...object, symbol: '1' });
     }, Error);
 });
 
 test('throw on required symbol, not provided', (t) => {
     t.throws(() => {
         const { requiredSymbol, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required number, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredSymbol: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredSymbol: '1' });
     }, Error);
 });
 
 test('pass on optional function, not provided', (t) => {
     t.notThrows(() => {
         const { func, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional function, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, func: '1' });
+        ArgTypes.check(argTypes, { ...object, func: '1' });
     }, Error);
 });
 
 test('throw on required function, not provided', (t) => {
     t.throws(() => {
         const { requiredFunc, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required function, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredFunc: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredFunc: '1' });
     }, Error);
 });
 
 test('pass on optional object, not provided', (t) => {
     t.notThrows(() => {
         const { object: _, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional object, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, object: '1' });
+        ArgTypes.check(argTypes, { ...object, object: '1' });
     }, Error);
 });
 
 test('throw on required object, not provided', (t) => {
     t.throws(() => {
         const { requiredObject, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required object, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredObject: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredObject: '1' });
     }, Error);
 });
 
 test('pass on optional array, not provided', (t) => {
     t.notThrows(() => {
         const { array, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throw on optional array, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, array: '1' });
+        ArgTypes.check(argTypes, { ...object, array: '1' });
     }, Error);
 });
 
 test('throw on required array, not provided', (t) => {
     t.throws(() => {
         const { requiredArray, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required array, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredArray: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredArray: '1' });
     }, Error);
 });
 
 test('pass on optional shape, not provided', (t) => {
     t.notThrows(() => {
         const { shape, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on optional shape, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, shape: 1 });
+        ArgTypes.check(argTypes, { ...object, shape: 1 });
     }, Error);
 });
 
 test('throws on optional shape, array type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, shape: [] });
+        ArgTypes.check(argTypes, { ...object, shape: [] });
     }, Error);
 });
 
 test('throws on required shape, not provided', (t) => {
     t.throws(() => {
         const { requiredShape, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required shape, nested type, required number, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredShape: { requiredNumber: '1' } });
+        ArgTypes.check(argTypes, { ...object, requiredShape: { requiredNumber: '1' } });
     }, Error);
 });
 
 test('pass on optional instanceOf, not provided', (t) => {
     t.notThrows(() => {
         const { instanceOf, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on optional instanceOf, number type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, instanceOf: 1 });
+        ArgTypes.check(argTypes, { ...object, instanceOf: 1 });
     }, Error);
 });
 
 test('throws on required instanceOf, not provided', (t) => {
     t.throws(() => {
         const { requiredInstanceOf, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required instanceOf, string type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredInstanceOf: '1' });
+        ArgTypes.check(argTypes, { ...object, requiredInstanceOf: '1' });
     }, Error);
 });
 
 test('pass on optional oneOf, not provided', (t) => {
     t.notThrows(() => {
         const { oneOf, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on optional oneOf, not included value', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, oneOf: 3 });
+        ArgTypes.check(argTypes, { ...object, oneOf: 3 });
     }, Error);
 });
 
 test('throws on required oneOf, not provided', (t) => {
     t.throws(() => {
         const { requiredOneOf, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required oneOf, not included value', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredOneOf: 3 });
+        ArgTypes.check(argTypes, { ...object, requiredOneOf: 3 });
     }, Error);
 });
 
 test('pass on optional oneOfType, not provided', (t) => {
     t.notThrows(() => {
         const { oneOfType, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on optional oneOfType, not included type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, oneOfType: {} });
+        ArgTypes.check(argTypes, { ...object, oneOfType: {} });
     }, Error);
 });
 
 test('throws on required oneOfType, not provided', (t) => {
     t.throws(() => {
         const { requiredOneOfType, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required oneOfType, not included type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredOneOfType: {} });
+        ArgTypes.check(argTypes, { ...object, requiredOneOfType: {} });
     }, Error);
 });
 
 test('pass on optional arrayOfType, not provided', (t) => {
     t.notThrows(() => {
         const { arrayOfType, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on optional arrayOfType, not included type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, arrayOfType: [1, '2'] });
+        ArgTypes.check(argTypes, { ...object, arrayOfType: [1, '2'] });
     }, Error);
 });
 
 test('throws on required arrayOfType, not provided', (t) => {
     t.throws(() => {
         const { requiredArrayOfType, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required arrayOfType, not included type', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { ...object, requiredArrayOfType: [1, '2'] });
+        ArgTypes.check(argTypes, { ...object, requiredArrayOfType: [1, '2'] });
     }, Error);
 });
 
 test('pass on optional any, not provided', (t) => {
     t.notThrows(() => {
         const { any, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
 test('throws on required any, not provided', (t) => {
     t.throws(() => {
         const { requiredAny, ...rest } = object;
-        PropTypes.check(propTypes, rest);
+        ArgTypes.check(argTypes, rest);
     }, Error);
 });
 
-test('throw on custom props, not passed', (t) => {
+test('throw on custom args, not passed', (t) => {
     t.throws(() => {
-        PropTypes.check(propTypes, { object, custom: 1 });
+        ArgTypes.check(argTypes, { object, custom: 1 });
     }, Error);
+});
+
+test('merge first layer object', (t) => {
+    t.deepEqual(ArgTypes.merge({ a: 1, b: 2 }, { c: 3 }), { a: 1, b: 2, c: 3 });
+});
+
+test('merge second layer object', (t) => {
+    t.deepEqual(ArgTypes.merge({ a: 1, b: 2, deep: { e: 4, f: 5 } }, { c: 3, deep: { g: 6 } }), {
+        a: 1, b: 2, c: 3, deep: {
+            e: 4, f: 5, g: 6
+        }
+    });
 });
