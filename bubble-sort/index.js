eda367fab071f6c0cb43ada897669a405fd6f0db
vivaxy <xyxuye2007@126.com>
2018-02-26 11:18:20 +0800

    fix(quick sort): :bug:Fix quick sort animation not finished
    And fix bubble sort error

--- bubble-sort/index.js
+++ bubble-sort/index.js
@@ -1,40 +1,40 @@
 // var
-var getQueryStringByName = function (name) {
-        var result = location.search.match(new RegExp('[\?\&]' + name + '=([^\&]+)', 'i'));
-        if (result == null || result.length < 1) {
-            return '';
+function getQueryStringByName(name, defaultValue) {
+    const result = location.search.match(new RegExp('[\?\&]' + name + '=([^\&]+)', 'i'));
+    if (result === null || result.length < 1) {
+        if (defaultValue !== undefined) {
+            return defaultValue;
         }
-        return result[1];
-    },
-    interval = getQueryStringByName('interval'),
-    number = getQueryStringByName('number');
-if (interval == '') {
-    interval = 300;
+        return null;
+    }
+    return result[1];
 }
-if (number == '') {
-    number = 30;
-}
+
+const interval = getQueryStringByName('interval', 300);
+const number = getQueryStringByName('number', 30);
 // main
-var array = [],
-    step = [],
-    stepN = 0,
-    a = [],
-    i;
+const array = [];
+const step = [];
+let stepN = 0;
+const a = [];
+let i;
 for (i = 0; i < number; i++) {
     array[i] = Math.random();
     a[i] = array[i];
 }
-var putArrayInStep = function (c, m) {
+
+function putArrayInStep(c, m) {
     step[stepN] = {
         compare: c,
         move: m
     };
     stepN++;
-};
+}
+
 for (i = 0; i < a.length - 1; i++) {
-    for (var j = 0; j < a.length - 1 - i; j++) {
+    for (let j = 0; j < a.length - 1 - i; j++) {
         if (a[j] > a[j + 1]) {
-            var t = a[j];
+            const t = a[j];
             a[j] = a[j + 1];
             a[j + 1] = t;
             putArrayInStep(j, 1);
@@ -43,68 +43,75 @@ for (i = 0; i < a.length - 1; i++) {
         }
     }
 }
+
 // css
-var getCssName = function (name) {
-        var prefixes = ['', '-webkit-', '-ms-', '-moz-'];
-        var target = document.documentElement.style;
-        for (var i = 0; i < prefixes.length; i++) {
-            var test = prefixes[i] + name;
-            if (test in target) {
-                return test;
-            }
+function getCssName(name) {
+    const prefixes = ['', '-webkit-', '-ms-', '-moz-'];
+    const target = document.documentElement.style;
+    for (let i = 0; i < prefixes.length; i++) {
+        const test = prefixes[i] + name;
+        if (test in target) {
+            return test;
         }
-        return null;
-    },
-    transition = getCssName('transition'),
-    css = document.styleSheets[0],
-    addCss = function (sheet, selectorText, cssText, position) {
-        var pos = position || sheet.cssRules.length;
-        if (sheet.insertRule) {
-            sheet.insertRule(selectorText + '{' + cssText + '}', pos);
-        } else if (sheet.addRule) {//IE
-            sheet.addRule(selectorText, cssText, pos);
-        }
-    };
+    }
+    return null;
+}
+
+const transition = getCssName('transition');
+const css = document.styleSheets[0];
+
+function addCss(sheet, selectorText, cssText, position) {
+    const pos = position || sheet.cssRules.length;
+    if (sheet.insertRule) {
+        sheet.insertRule(selectorText + '{' + cssText + '}', pos);
+    } else if (sheet.addRule) { // IE
+        sheet.addRule(selectorText, cssText, pos);
+    }
+}
+
 addCss(css, 'div', 'width: ' + 100 / number + '%; ' + transition + ': background ' + interval / 1000 + 's, left ' + interval / 1000 + 's;');
 for (i = 0; i < number; i++) {
     addCss(css, '.div' + i, 'height: ' + 100 * array[i] + '%;');
 }
 // create div
-var div = [];
+const div = [];
 for (i = 0; i < number; i++) {
-    var d = document.createElement('div');
+    const d = document.createElement('div');
     d.classList.add('div' + i);
     d.style.left = 100 / number * i + '%';
     document.body.appendChild(d);
     div.push(d);
 }
 // animate
-var stepI = -1,
-    compare = function () {
-        if (stepI >= 0) {
-            div[step[stepI].compare].classList.remove('compare');
-            div[step[stepI].compare + 1].classList.remove('compare');
-        }
-        stepI++;
-        if (stepI > stepN) {
-            clearTimeout(loop1);
-            clearTimeout(loop2);
-            return;
-        }
-        div[step[stepI].compare].classList.add('compare');
-        div[step[stepI].compare + 1].classList.add('compare');
-        loop2 = setTimeout(move, interval);
-    },
-    move = function () {
-        if (step[stepI].move == 1) {
-            var t = div[step[stepI].compare].style.left;
-            div[step[stepI].compare].style.left = div[step[stepI].compare + 1].style.left;
-            div[step[stepI].compare + 1].style.left = t;
-            var divT = div[step[stepI].compare];
-            div[step[stepI].compare] = div[step[stepI].compare + 1];
-            div[step[stepI].compare + 1] = divT;
-        }
-        loop1 = setTimeout(compare, interval);
-    },
-    loop2 = 0,
+let stepI = -1;
+
+function compare() {
+    if (stepI >= 0) {
+        div[step[stepI].compare].classList.remove('compare');
+        div[step[stepI].compare + 1].classList.remove('compare');
+    }
+    stepI++;
+    if (stepI >= stepN) {
+        clearTimeout(loop1);
+        clearTimeout(loop2);
+        return;
+    }
+    div[step[stepI].compare].classList.add('compare');
+    div[step[stepI].compare + 1].classList.add('compare');
+    loop2 = setTimeout(move, interval);
+}
+
+function move() {
+    if (step[stepI].move === 1) {
+        const t = div[step[stepI].compare].style.left;
+        div[step[stepI].compare].style.left = div[step[stepI].compare + 1].style.left;
+        div[step[stepI].compare + 1].style.left = t;
+        const divT = div[step[stepI].compare];
+        div[step[stepI].compare] = div[step[stepI].compare + 1];
+        div[step[stepI].compare + 1] = divT;
+    }
     loop1 = setTimeout(compare, interval);
+}
+
+let loop2 = 0;
+let loop1 = setTimeout(compare, interval);
--- quick-sort/index.js
+++ quick-sort/index.js
@@ -1,147 +1,154 @@
-// TODO fix sort not finished on screen
 // var
-var getQueryStringByName = function (name) {
-        var result = location.search.match(new RegExp('[?&]' + name + '=([^&]+)', 'i'));
-        if (result == null || result.length < 1) {
-            return '';
+function getQueryStringByName(name, defaultValue) {
+    const result = location.search.match(new RegExp('[?&]' + name + '=([^&]+)', 'i'));
+    if (result === null || result.length < 1) {
+        if (defaultValue !== undefined) {
+            return defaultValue;
         }
-        return result[1];
-    },
-    interval = getQueryStringByName('interval'),
-    number = getQueryStringByName('number');
-if (interval === '') {
-    interval = 10;
-}
-if (number === '') {
-    number = 100;
+        return null;
+    }
+    return result[1];
 }
 
+const interval = getQueryStringByName('interval', 10);
+const number = getQueryStringByName('number', 100);
+
 // define function
-var i = 0,
-    steps = [], // select compare move
-    quickSort = function (a, start, end) {
-        i = start;
-        var j = end;
-        steps.push({select: [i, j]});
-        while (i < j) {
-            steps.push({compareTo: i});
-            while (i < j && a[i] <= a[j]) {
-                steps.push({compare: j});
-                j--;
-            }
-            if (i < j) {
-                steps.push({compare: j});
-                steps.push({move: j});
-                var temp1 = a[i];
-                a[i] = a[j];
-                a[j] = temp1;
-            }
-            steps.push({compareTo: j});
-            while (i < j && a[i] < a[j]) {
-                steps.push({compare: i});
-                i++;
-            }
-            if (i < j) {
-                steps.push({compare: i});
-                steps.push({move: i});
-                var temp2 = a[i];
-                a[i] = a[j];
-                a[j] = temp2;
-            }
+let i = 0;
+const steps = []; // select compare move
+function quickSort(a, start, end) {
+    i = start;
+    let j = end;
+    steps.push({ select: [i, j] });
+    while (i < j) {
+        steps.push({ compareTo: i });
+        while (i < j && a[i] <= a[j]) {
+            steps.push({ compare: j });
+            j--;
         }
-        if (start < i - 1) quickSort(a, start, i - 1);
-        if (end > i + 1) quickSort(a, i + 1, end);
-        return a;
-    },
+        if (i < j) {
+            steps.push({ compare: j });
+            steps.push({ move: j });
+            const temp1 = a[i];
+            a[i] = a[j];
+            a[j] = temp1;
+        }
+        steps.push({ compareTo: j });
+        while (i < j && a[i] < a[j]) {
+            steps.push({ compare: i });
+            i++;
+        }
+        if (i < j) {
+            steps.push({ compare: i });
+            steps.push({ move: i });
+            const temp2 = a[i];
+            a[i] = a[j];
+            a[j] = temp2;
+        }
+    }
+    if (start < i - 1) quickSort(a, start, i - 1);
+    if (end > i + 1) quickSort(a, i + 1, end);
+    return a;
+}
+
 // define array
-    array = [], arrayInit = [];
+const array = [];
+const arrayInit = [];
 for (i = 0; i < number; i++) {
     array[i] = Math.random();
     arrayInit[i] = array[i];
 }
 // main
 quickSort(array, 0, array.length - 1);
+
 // css
-var getCssName = function (name) {
-        var prefixes = ['', '-webkit-', '-ms-', '-moz-'];
-        var target = document.documentElement.style;
-        for (var i = 0; i < prefixes.length; i++) {
-            var test = prefixes[i] + name;
-            if (test in target) {
-                return test;
-            }
+function getCssName(name) {
+    const prefixes = ['', '-webkit-', '-ms-', '-moz-'];
+    const target = document.documentElement.style;
+    for (let i = 0; i < prefixes.length; i++) {
+        const test = prefixes[i] + name;
+        if (test in target) {
+            return test;
         }
-        return null;
-    },
-    transition = getCssName('transition'),
-    css = document.styleSheets[0],
-    addCss = function (sheet, selectorText, cssText, position) {
-        var pos = position || sheet.cssRules.length;
-        if (sheet.insertRule) {
-            sheet.insertRule(selectorText + '{' + cssText + '}', pos);
-        } else if (sheet.addRule) {//IE
-            sheet.addRule(selectorText, cssText, pos);
-        }
-    };
+    }
+    return null;
+}
+
+const transition = getCssName('transition');
+const css = document.styleSheets[0];
+
+function addCss(sheet, selectorText, cssText, position) {
+    const pos = position || sheet.cssRules.length;
+    if (sheet.insertRule) {
+        sheet.insertRule(selectorText + '{' + cssText + '}', pos);
+    } else if (sheet.addRule) { // IE
+        sheet.addRule(selectorText, cssText, pos);
+    }
+}
+
 addCss(css, 'div', 'width: ' + 100 / number + '%; ' + transition + ': background ' + interval / 1000 + 's, left ' + interval / 1000 + 's;');
 for (i = 0; i < number; i++) {
     addCss(css, '.div' + i, 'height: ' + 100 * arrayInit[i] + '%;');
 }
 // create div
-var div = [];
+const div = [];
 for (i = 0; i < number; i++) {
-    var d = document.createElement('div');
+    const d = document.createElement('div');
     d.classList.add('div' + i);
     d.style.left = 100 / number * i + '%';
     document.body.appendChild(d);
     div.push(d);
 }
 // animate
-var toBeMoved = 0, loopVar = 0,
-    loopFunc = function () {
-        if (loopVar >= steps.length) {
-            clearTimeout(loopVar);
-            for (i = 0; i < number; i++) {
-                var classList = div[i].classList;
-                classList.remove('select');
-                classList.remove('compare');
-                classList.remove('compareTo');
-            }
-            return;
+let toBeMoved = 0;
+let loopVar = 0;
+
+function loopFunc() {
+    if (loopVar >= steps.length) {
+        clearTimeout(loopVar);
+        for (i = 0; i < number; i++) {
+            const classList = div[i].classList;
+            classList.remove('select');
+            classList.remove('compare');
+            classList.remove('compareTo');
         }
-        if (steps[loopVar].hasOwnProperty('select')) {
-            for (i = 0; i < number; i++) {
-                classList = div[i].classList;
-                classList.remove('select');
-                classList.remove('compare');
-                classList.remove('compareTo');
-            }
-            for (i = steps[loopVar].select[0]; i <= steps[loopVar].select[1]; i++) {
-                div[i].classList.add('select');
-            }
-            toBeMoved = steps[loopVar].select[0];
+        return;
+    }
+    if (steps[loopVar].hasOwnProperty('select')) {
+        for (i = 0; i < number; i++) {
+            const classList = div[i].classList;
+            classList.remove('select');
+            classList.remove('compare');
+            classList.remove('compareTo');
         }
-        if (steps[loopVar].hasOwnProperty('compareTo')) {
-            for (i = 0; i < number; i++) {
-                div[i].classList.remove('compareTo');
-            }
-            div[steps[loopVar].compareTo].classList.add('compareTo');
+        for (i = steps[loopVar].select[0]; i <= steps[loopVar].select[1]; i++) {
+            div[i].classList.add('select');
         }
-        if (steps[loopVar].hasOwnProperty('compare')) {
-            for (i = 0; i < number; i++) {
-                div[i].classList.remove('compare');
-            }
-            div[steps[loopVar].compare].classList.add('compare');
+        toBeMoved = steps[loopVar].select[0];
+    }
+    if (steps[loopVar].hasOwnProperty('compareTo')) {
+        for (i = 0; i < number; i++) {
+            div[i].classList.remove('compareTo');
         }
-        if (steps[loopVar].hasOwnProperty('move')) {
-            var temp = div[toBeMoved].style.left;
-            div[toBeMoved].style.left = div[steps[loopVar].move].style.left;
-            div[steps[loopVar].move].style.left = temp;
-            var divTemp = div[toBeMoved];
-            div[toBeMoved] = div[steps[loopVar].move];
-            div[steps[loopVar].move] = divTemp;
-            toBeMoved = steps[loopVar].move;
+        div[steps[loopVar].compareTo].classList.add('compareTo');
+    }
+    if (steps[loopVar].hasOwnProperty('compare')) {
+        for (i = 0; i < number; i++) {
+            div[i].classList.remove('compare');
         }
-        loopVar = setTimeout(loopFunc, interval);
-    };
+        div[steps[loopVar].compare].classList.add('compare');
+    }
+    if (steps[loopVar].hasOwnProperty('move')) {
+        const temp = div[toBeMoved].style.left;
+        div[toBeMoved].style.left = div[steps[loopVar].move].style.left;
+        div[steps[loopVar].move].style.left = temp;
+        const divTemp = div[toBeMoved];
+        div[toBeMoved] = div[steps[loopVar].move];
+        div[steps[loopVar].move] = divTemp;
+        toBeMoved = steps[loopVar].move;
+    }
+    loopVar++;
+    setTimeout(loopFunc, interval);
+}
+
 loopFunc();
