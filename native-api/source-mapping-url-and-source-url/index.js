5e3bf39fe1fa1ef8489e74957ada6e678c4f2cab
vivaxy <xyxuye2007@126.com>
2018-03-09 17:10:27 +0800

    feat(native-api): :sparkles:source-mapping-url-and-source-url

---
+++ native-api/source-mapping-url-and-source-url/index.js
@@ -0,0 +1,10 @@
+/**
+ * @since 20180309 16:49
+ * @author vivaxy
+ */
+
+//# sourceURL=course:///mock.js
+//# sourceMappingURL=http://172.24.236.146:3001/index.js.map
+
+const func = new Function('console.log(0); //# sourceURL=course:///console.js');
+func();
