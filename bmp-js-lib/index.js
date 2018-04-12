0ec65c6c863f1128d1562d1aa4ace822982714ff
vivaxy <xyxuye2007@126.com>
2018-04-12 18:47:30 +0800

    feat(bmp-js-lib): :sparkles:

---
+++ bmp-js-lib/index.js
@@ -0,0 +1,25 @@
+/**
+ * @since 20180412 17:20
+ * @author vivaxy
+ */
+
+const fse = require('fs-extra');
+const bmp = require('bmp-js');
+
+const imageDataToBinary1 = async() => {
+  const frameData = [
+    0xFF, 0x00, 0x00, 0x80,
+    0x00, 0xFF, 0x00, 0x80,
+    0x00, 0x00, 0xFF, 0x80,
+    0xFF, 0x00, 0xFF, 0x00,
+  ];
+  const bmpImageData = bmp.encode({
+    width: 2,
+    height: 2,
+    data: frameData,
+  }, 100);
+  console.log(bmpImageData);
+  await fse.writeFile('imageDataToBinary1.jpg', bmpImageData.data, { encoding: 'binary' });
+};
+
+imageDataToBinary1();
