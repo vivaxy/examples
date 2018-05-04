b998445d0eaed12dbdb54ba5479aee328e63398c
vivaxy <xyxuye2007@126.com>
2018-05-04 18:44:50 +0800

    feat(compiler): :sparkles:javascript

--- compiler/javascript/__tests__/index.js
+++ compiler/javascript/__tests__/index.js
@@ -6,7 +6,10 @@
 const test = require('ava');
 const compiler = require('../index.js');
 const tokenTypes = compiler.tokenTypes;
+const astTypes = compiler.astTypes;
 const tokenizer = compiler.tokenizer;
+const parser = compiler.parser;
+const execute = compiler.execute;
 
 test('tokenizer', (t) => {
   t.deepEqual(tokenizer('{{userGrade}} == 0'), [
@@ -20,7 +23,7 @@ test('tokenizer', (t) => {
     },
     {
       type: tokenTypes.NUMBER,
-      value: '0',
+      value: 0,
     },
   ]);
 
@@ -39,3 +42,72 @@ test('tokenizer', (t) => {
     },
   ]);
 });
+
+test('parser', (t) => {
+  t.deepEqual(parser(tokenizer('1 === 2')), {
+    type: astTypes.PROGRAM,
+    body: [
+      {
+        type: astTypes.EXPRESSION_STATEMENT,
+        value: 1,
+      },
+      {
+        type: astTypes.BINARY_OPERATOR,
+        value: '===',
+      },
+      {
+        type: astTypes.NUMBER_LITERAL,
+        value: 2,
+      },
+    ],
+  });
+  t.deepEqual(parser(tokenizer('1 === 1 && (1 === 2)')), {
+    type: astTypes.PROGRAM,
+    body: [
+      {
+        type: astTypes.NUMBER_LITERAL,
+        value: 1,
+      },
+      {
+        type: astTypes.BINARY_OPERATOR,
+        value: '===',
+      },
+      {
+        type: astTypes.NUMBER_LITERAL,
+        value: 1,
+      },
+      {
+        type: astTypes.BINARY_OPERATOR,
+        value: '&&',
+      },
+      {
+        type: astTypes.PARENTHESIS,
+        body: [
+          {
+            type: astTypes.NUMBER_LITERAL,
+            value: 1,
+          },
+          {
+            type: astTypes.BINARY_OPERATOR,
+            value: '===',
+          },
+          {
+            type: astTypes.NUMBER_LITERAL,
+            value: 2,
+          },
+        ],
+      },
+    ],
+  });
+});
+
+// test('execute', (t) => {
+  // t.deepEqual(execute(parser(tokenizer('1 === 1 && (1 === 2)'))), false);
+  // t.deepEqual(execute(parser(tokenizer('1 === 1 && 1 === 2'))), false);
+  // t.deepEqual(execute(parser(tokenizer('{{userGrade}} > 1'), { userGrade: 1 })), false);
+  // t.deepEqual(execute(parser(tokenizer('{{userGrade}} >= 1'), { userGrade: 1 })), true);
+  // t.deepEqual(execute(parser(tokenizer('{{userGrade}} >= 1 && {{userName}} === \'test\''), {
+  //   userGrade: 1,
+  //   userName: 'test',
+  // })), true);
+// });
--- compiler/javascript/index.js
+++ compiler/javascript/index.js
@@ -5,22 +5,78 @@
 
 const tokenTypes = {
   ARITHMETIC_OPERATOR: 'arithmeticOperator', // +(二元), -(二元), /, *, %, **, ++, --, -(一元), +(一元)
-  BITWISE_OPERATOR: 'bitwiseOperator', // &, |, ^, ~, <<, >>, >>>
+  // BITWISE_OPERATOR: 'bitwiseOperator', // &, |, ^, ~, <<, >>, >>>
   COMPARISON_OPERATOR: 'comparisonOperator', // ==, ===, >, <, <=, >=, !=, !==
   CONDITIONAL_OPERATOR: 'conditionalOperator', // ? :
   LOGICAL_OPERATOR: 'logicalOperator', // &&, ||, !
   ARGUMENT: 'argument',
   NUMBER: 'number',
   STRING: 'string',
+  BOOLEAN: 'boolean',
   PARENTHESIS: 'parenthesis',
 };
 
 compiler.tokenTypes = tokenTypes;
 
+const astTypes = {
+  PROGRAM: 'Program',
+  EXPRESSION_STATEMENT: 'ExpressionStatement',
+  LITERAL: 'Literal',
+  BINARY_EXPRESSION: 'BinaryExpression',
+  UNARY_EXPRESSION: 'UnaryExpression',
+  LOGICAL_EXPRESSION: 'LogicalExpression',
+};
+
+compiler.astTypes = astTypes;
+
+const astFactory = {
+  PROGRAM: (body) => {
+    return { type: astTypes.PROGRAM, body };
+  },
+  EXPRESSION_STATEMENT: (expression) => {
+    return { type: astTypes.EXPRESSION_STATEMENT, expression };
+  },
+  LITERAL: (value) => {
+    return { type: astTypes.LITERAL, value };
+  },
+  BINARY_EXPRESSION: (operator, left, right) => {
+    return { type: astTypes.BINARY_EXPRESSION, operator, left, right };
+  },
+  UNARY_EXPRESSION: (operator, argument) => {
+    return { type: astTypes.UNARY_EXPRESSION, operator, argument };
+  },
+  LOGICAL_EXPRESSION: (operator, left, right) => {
+    return { type: astTypes.LOGICAL_EXPRESSION, operator, left, right };
+  },
+};
+
+compiler.astFactory = astFactory;
+
 function tokenizer(input) {
   let tokens = [];
   let i = 0;
   const length = input.length;
+
+  function pushToken(type, value, extraLength) {
+    if (type === tokenTypes.NUMBER) {
+      value = Number(value);
+    }
+    if (type === tokenTypes.BOOLEAN) {
+      if (value !== 'true' && value !== 'false') {
+        throw new Error('Error BOOLEAN token: ' + value);
+      }
+      value = value === 'true';
+    }
+    if (!extraLength) {
+      extraLength = 0;
+    }
+    tokens.push({
+      type,
+      value,
+    });
+    i += value.length + extraLength;
+  }
+
   while (i < length) {
     let char = input[i];
     if (char === ' ') {
@@ -30,124 +86,84 @@ function tokenizer(input) {
     if (char === '*' || char === '+' || char === '-') {
       const nextChar = char[i + 1];
       if (char === nextChar) {
-        tokens.push({ type: tokenTypes.ARITHMETIC_OPERATOR, value: char + char });
-        i += 2;
+        pushToken(tokenTypes.ARITHMETIC_OPERATOR, char + char);
         continue;
       }
-      tokens.push({ type: tokenTypes.ARITHMETIC_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.ARITHMETIC_OPERATOR, char);
       continue;
     }
     if (char === '/' || char === '%') {
-      tokens.push({ type: tokenTypes.ARITHMETIC_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.ARITHMETIC_OPERATOR, char);
       continue;
     }
     if (char === '&' || char === '|') {
       const nextChar = input[i + 1];
       if (char === nextChar) {
-        tokens.push({ type: tokenTypes.LOGICAL_OPERATOR, value: char + char });
-        i += 2;
+        pushToken(tokenTypes.LOGICAL_OPERATOR, char + char);
         continue;
       }
-      tokens.push({ type: tokenTypes.BITWISE_OPERATOR, value: char });
-      i++;
-      continue;
-    }
-    if (char === '^' || char === '~') {
-      tokens.push({ type: tokenTypes.BITWISE_OPERATOR, value: char });
-      i++;
-      continue;
     }
     if (char === '<') {
       const nextChar = input[i + 1];
-      if (nextChar === '<') {
-        tokens.push({ type: tokenTypes.BITWISE_OPERATOR, value: '<<' });
-        i += 2;
-        continue;
-      }
       if (nextChar === '=') {
-        tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '<=' });
-        i += 2;
+        pushToken(tokenTypes.COMPARISON_OPERATOR, '<=');
         continue;
       }
-      tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.COMPARISON_OPERATOR, char);
       continue;
     }
     if (char === '>') {
       const nextChar = input[i + 1];
-      if (nextChar === '>') {
-        if (input[i + 2] === '>') {
-          tokens.push({ type: tokenTypes.BITWISE_OPERATOR, value: '>>>' });
-          i += 3;
-          continue;
-        }
-        tokens.push({ type: tokenTypes.BITWISE_OPERATOR, value: '>>' });
-        i += 2;
-        continue;
-      }
       if (nextChar === '=') {
-        tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '>=' });
-        i += 2;
+        pushToken(tokenTypes.COMPARISON_OPERATOR, '>=');
         continue;
       }
-      tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.COMPARISON_OPERATOR, char);
       continue;
     }
     if (char === '=') {
       const nextChar = input[i + 1];
       if (nextChar === '=') {
         if (input[i + 2] === '=') {
-          tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '===' });
-          i += 3;
+          pushToken(tokenTypes.COMPARISON_OPERATOR, '===');
           continue;
         }
-        tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '==' });
-        i += 2;
+        pushToken(tokenTypes.COMPARISON_OPERATOR, '==');
         continue;
       }
-      tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.COMPARISON_OPERATOR, char);
       continue;
     }
     if (char === '!') {
       const nextChar = input[i + 1];
       if (nextChar === '=') {
         if (input[i + 2] === '=') {
-          tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '!==' });
-          i += 3;
+          pushToken(tokenTypes.COMPARISON_OPERATOR, '!==');
           continue;
         }
-        tokens.push({ type: tokenTypes.COMPARISON_OPERATOR, value: '!=' });
-        i += 2;
+        pushToken(tokenTypes.COMPARISON_OPERATOR, '!=');
         continue;
       }
-      tokens.push({ type: tokenTypes.LOGICAL_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.LOGICAL_OPERATOR, char);
       continue;
     }
     if (char === '?' || char === ':') {
-      tokens.push({ type: tokenTypes.CONDITIONAL_OPERATOR, value: char });
-      i++;
+      pushToken(tokenTypes.CONDITIONAL_OPERATOR, char);
       continue;
     }
     if (char === '{') {
       if (input[i + 1] === '{') {
-        i += 2;
-        char = input[i];
-        let argumentName = '';
+        let j = 2;
+        char = input[i + j];
+        let value = '';
         while (i < length && char !== '}') {
-          argumentName += char;
-          i++;
-          char = input[i];
+          value += char;
+          j++;
+          char = input[i + j];
         }
-        i++;
-        if (input[i] === '}') {
-          if (argumentName !== '') {
-            tokens.push({ type: tokenTypes.ARGUMENT, value: argumentName });
-            i++;
+        if (input[i + j + 1] === '}') {
+          if (value !== '') {
+            pushToken(tokenTypes.ARGUMENT, value, 4);
             continue;
           }
         }
@@ -156,75 +172,363 @@ function tokenizer(input) {
     const NUMBERS = /[0-9]/;
     if (NUMBERS.test(char)) {
       let value = '';
+      let j = 0;
       while (NUMBERS.test(char)) {
         value += char;
-        char = input[++i];
+        j++;
+        char = input[i + j];
       }
-      tokens.push({ type: tokenTypes.NUMBER, value });
+      pushToken(tokenTypes.NUMBER, value);
       continue;
     }
     if (char === '"') {
       let value = '';
-      char = input[++i];
+      let j = 1;
+      char = input[i + j];
       while (char !== '"') {
         value += char;
-        char = input[++i];
+        j++;
+        char = input[i + j];
       }
-      char = input[++i];
-      tokens.push({ type: tokenTypes.STRING, value });
+      pushToken(tokenTypes.STRING, value, 2);
       continue;
     }
     if (char === '\'') {
       let value = '';
-      char = input[++i];
+      let j = 1;
+      char = input[i + j];
       while (char !== '\'') {
         value += char;
-        char = input[++i];
+        j++;
+        char = input[i + j];
       }
-      char = input[++i];
-      tokens.push({ type: tokenTypes.STRING, value });
+      pushToken(tokenTypes.STRING, value, 2);
       continue;
     }
-    if (char === ')' || char === ')') {
-      tokens.push({ type: tokenTypes.PARENTHESIS, value });
-      i++;
+    if (char === '(' || char === ')') {
+      pushToken(tokenTypes.PARENTHESIS, char);
+      continue;
+    }
+    if (char === 't' && input[i + 1] === 'r' && input[i + 2] === 'u' && input[i + 3] === 'e') {
+      pushToken(tokenTypes.BOOLEAN, 'true');
+      continue;
+    }
+    if (char === 'f' && input[i + 1] === 'a' && input[i + 2] === 'l' && input[i + 3] === 's' && input[i + 4] === 'e') {
+      pushToken(tokenTypes.BOOLEAN, 'false');
       continue;
     }
     throw new Error('Unexpected token: ' + char);
   }
   return tokens;
 }
+
 compiler.tokenizer = tokenizer;
 
-function parser(input) {
-  return input;
+function parser(tokens, args) {
+  const ast = astFactory.PROGRAM([]);
+
+  let length = tokens.length;
+
+  ast.body.push(walk(0, length - 1));
+
+  function getLogicalExpressionIndex(start, end) {
+    for (let i = end; i >= start; i--) {
+      if (
+        tokens[i].type === tokenTypes.LOGICAL_OPERATOR
+        && tokens[i].value === '&&'
+        && tokens[i].value === '||'
+      ) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  function getBinaryExpressionIndex(start, end) {
+    for (let i = end; i >= start; i--) {
+      if (
+        (
+          tokens[i].type === tokenTypes.ARITHMETIC_OPERATOR
+          && tokens[i - 1]
+          && (
+            tokens[i - 1].type === tokenTypes.NUMBER
+            || tokens[i - 1].type === tokenTypes.STRING
+            || tokens[i - 1].type === tokenTypes.BOOLEAN
+          )
+        ) || (
+          tokens[i].type === tokenTypes.COMPARISON_OPERATOR
+        )
+      ) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  function walk(start, end) {
+    let logicalIndex = getLogicalExpressionIndex(start, end);
+    let expression;
+    if (logicalIndex === -1) {
+        let binaryExpressionIndex = getBinaryExpressionIndex(start, end);
+        expression = astFactory.BINARY_EXPRESSION();
+    } else {
+      let leftExpression = null;
+      let rightExpression = null;
+      expression = astFactory.LOGICAL_EXPRESSION(tokens[logicalIndex].value, leftExpression, rightExpression);
+    }
+    return astFactory.EXPRESSION_STATEMENT(expression);
+
+    let token = tokens[i];
+    if (token.type === tokenTypes.NUMBER) {
+      i++;
+      return astFactory.NUMBER_LITERAL(token.value);
+    }
+    if (token.type === tokenTypes.STRING) {
+      i++;
+      return {
+        type: astTypes.STRING_LITERAL,
+        value: token.value,
+      };
+    }
+    if (token.type === tokenTypes.BOOLEAN) {
+      i++;
+      return {
+        type: astTypes.BOOLEAN_LITERAL,
+        value: token.value,
+      };
+    }
+    if (token.type === tokenTypes.ARGUMENT) {
+      i++;
+      const value = args[token.value];
+      if (typeof value === 'string') {
+        return {
+          type: astTypes.STRING_LITERAL,
+          value,
+        };
+      } else if (typeof value === 'boolean') {
+        return {
+          type: astTypes.BOOLEAN_LITERAL,
+          value,
+        };
+      } else if (typeof value === 'number') {
+        return {
+          type: astTypes.NUMBER_LITERAL,
+          value,
+        };
+      } else {
+        throw new Error('Unexpected argument: ' + token.value);
+      }
+    }
+    if (token.type === tokenTypes.COMPARISON_OPERATOR) {
+      i++;
+      return {
+        type: astTypes.BINARY_OPERATOR,
+        value: token.value,
+      };
+    }
+    if (token.type === tokenTypes.LOGICAL_OPERATOR) {
+      i++;
+      return {
+        type: astTypes.BINARY_OPERATOR,
+        value: token.value,
+      };
+    }
+    if (token.type === tokenTypes.PARENTHESIS && token.value === '(') {
+      let body = [];
+      token = tokens[++i];
+      while ((token.type !== tokenTypes.PARENTHESIS) || (token.type === tokenTypes.PARENTHESIS && token.value !== ')')) {
+        body.push(walk());
+        token = tokens[i];
+      }
+      i++;
+      return {
+        type: astTypes.PARENTHESIS,
+        body,
+      };
+    }
+    throw TypeError(token.type);
+  }
+
+  return ast;
 }
+
 compiler.parser = parser;
 
-function transformer(input) {
-  return input;
-}
-compiler.transformer = transformer;
+function execute(ast) {
+  function evaluate(nodes) {
+    let nodeArray = nodes.slice();
+    let prevLength = nodeArray.length + 1;
+    while (nodeArray.length > 1) {
+      // console.log(nodeArray);
+      if (prevLength === nodeArray.length) {
+        throw new Error('Unconsumable nodes');
+      }
+      prevLength = nodeArray.length;
+      const node0 = nodeArray[0];
+      const node1 = nodeArray[1];
+      const node2 = nodeArray[2];
+      if (node0.type === astTypes.PARENTHESIS) {
+        nodeArray.splice(0, 1, evaluate(node0.body));
+        prevLength++;
+      } else if (node2.type === astTypes.PARENTHESIS) {
+        nodeArray.splice(2, 1, evaluate(node2.body));
+        prevLength++;
+      } else if (
+        node0.type === astTypes.UNARY_OPERATOR
+        && (
+          node1.type === astTypes.NUMBER_LITERAL
+          || node1.type === astTypes.STRING_LITERAL
+          || node1.type === astTypes.BOOLEAN_LITERAL
+        )
+      ) {
+        nodeArray.splice(0, 2);
+        if (node0.value === '+') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: +node1.value,
+          });
+        } else if (node0.value === '-') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: -node1.value,
+          });
+        } else {
+          throw new Error('Unexpected UNARY_OPERATOR: ' + node0.value);
+        }
+      } else if (node0.type === astTypes.UNARY_OPERATOR && node1.type === astTypes.BOOLEAN_LITERAL) {
+        nodeArray.splice(0, 2);
+        if (node0.value === '!') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: !node1.value,
+          });
+        } else {
+          throw new Error('Unexpected UNARY_OPERATOR: ' + node0.value);
+        }
+      } else if (node0.type === astTypes.NUMBER_LITERAL && node1.type === astTypes.UNARY_OPERATOR) {
+        nodeArray.splice(0, 2);
+        if (node1.value === '++') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value++,
+          });
+        } else if (node1.value === '--') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value++,
+          });
+        } else {
+          throw new Error('Unexpected UNARY_OPERATOR: ' + node1.value);
+        }
+      } else if (
+        node2
+        && node0.type === astTypes.NUMBER_LITERAL
+        && node1.type === astTypes.BINARY_OPERATOR
+        && node2.type === astTypes.NUMBER_LITERAL
+      ) {
+        nodeArray.splice(0, 3);
+        if (node1.value === '+') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value + node2.value,
+          });
+        } else if (node1.value === '-') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value - node2.value,
+          });
+        } else if (node1.value === '*') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value * node2.value,
+          });
+        } else if (node1.value === '%') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value % node2.value,
+          });
+        } else if (node1.value === '**') {
+          nodeArray.unshift({
+            type: astTypes.NUMBER_LITERAL,
+            value: node0.value ** node2.value,
+          });
+        } else if (node1.value === '===') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value === node2.value,
+          });
+        } else if (node1.value === '==') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value == node2.value,
+          });
+        } else if (node1.value === '>') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value > node2.value,
+          });
+        } else if (node1.value === '<') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value < node2.value,
+          });
+        } else if (node1.value === '>=') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value >= node2.value,
+          });
+        } else if (node1.value === '<=') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value <= node2.value,
+          });
+        } else if (node1.value === '!=') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value != node2.value,
+          });
+        } else if (node1.value === '!==') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value !== node2.value,
+          });
+        } else {
+          throw new Error('Unexpected BINARY_OPERATOR: ' + node1.value);
+        }
+      } else if (
+        node2
+        && node0.type === astTypes.BOOLEAN_LITERAL
+        && node1.type === astTypes.BINARY_OPERATOR
+        && node2.type === astTypes.BOOLEAN_LITERAL
+      ) {
+        if (node1.value === '&&') {
+          nodeArray.splice(0, 3);
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value && node2.value,
+          });
+        } else if (node1.value === '||') {
+          nodeArray.unshift({
+            type: astTypes.BOOLEAN_LITERAL,
+            value: node0.value || node2.value,
+          });
+        }
+      } else {
+        throw new Error('Unexpected BINARY_OPERATOR: ' + node1.value);
+      }
+    }
+    return nodeArray[0];
+  }
 
-function codeGenerator(input) {
-  return input;
+  return evaluate(ast.body).value;
 }
-compiler.codeGenerator = codeGenerator;
+
+compiler.execute = execute;
 
 function compiler(input) {
-  let tokens = tokenizer(input);
-  // console.log(tokens);
-
-  let ast = parser(tokens);
-  // console.log(JSON.stringify(ast, null, 2));
-
-  let newAst = transformer(ast);
-  // console.log(JSON.stringify(newAst, null, 2));
-
-  let output = codeGenerator(newAst);
-  // console.log(output);
-
-  return output;
+  const tokens = tokenizer(input);
+  const ast = parser(tokens);
+  return execute(ast);
 }
 
 module.exports = compiler;
