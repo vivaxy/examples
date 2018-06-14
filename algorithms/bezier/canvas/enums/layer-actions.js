e387da1456415bb1e925f71712ad5a68929be9ac
vivaxy <xyxuye2007@126.com>
2018-06-14 20:30:01 +0800

    chore(algorithms/bezier):

--- algorithms/bezier/canvas/enums/layer-actions.js
+++
@@ -1,8 +0,0 @@
-/**
- * @since 20180614 10:22
- * @author vivaxy
- */
-
-export const FUNCTION = 'FUNCTION';
-export const PROPERTY = 'PROPERTY';
-export const SET_FONT = 'SET_FONT';
--- algorithms/bezier/canvas/enums/layer-indexes.js
+++
@@ -1,9 +0,0 @@
-/**
- * @since 20180614 10:05
- * @author vivaxy
- */
-
-export const CLEAR_CANVAS = 1;
-export const CURVE = 2;
-export const ACTIVE_CURVE = 3;
-export const BUTTON = 4;
