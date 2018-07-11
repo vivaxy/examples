0320b49cc4c514f582792f6dcbd97e3086f05c50
vivaxy <xyxuye2007@126.com>
2018-07-11 19:19:00 +0800

    chore:

--- algorithms/graph/dijkstra/implements/__tests__/index.js
+++ algorithms/graph/dijkstra/implements/__tests__/index.js
@@ -9,7 +9,7 @@ const Graph = require('../../../implements/class/Graph.js');
 const GraphEdge = require('../../../implements/class/GraphEdge.js');
 const GraphVertex = require('../../../implements/class/GraphVertex.js');
 
-test('dijkstra', (t) => {
+test('find minimum paths to all vertices for undirected graph', (t) => {
 
   const vertexA = new GraphVertex('A');
   const vertexB = new GraphVertex('B');
@@ -33,7 +33,7 @@ test('dijkstra', (t) => {
   const edgeFG = new GraphEdge(vertexF, vertexG, 3);
   const edgeEG = new GraphEdge(vertexE, vertexG, 5);
 
-  const graph = new Graph();
+  const graph = new Graph(false);
   graph
     .addVertex(vertexH)
     .addEdge(edgeAB)
@@ -69,5 +69,58 @@ test('dijkstra', (t) => {
   t.is(previousVertices.C.getKey(), 'A');
   t.is(previousVertices.A, null);
   t.is(previousVertices.H, null);
+
+});
+
+
+test('find minimum paths to all vertices for directed graph with negative edge weights', (t) => {
+
+  const vertexS = new GraphVertex('S');
+  const vertexE = new GraphVertex('E');
+  const vertexA = new GraphVertex('A');
+  const vertexD = new GraphVertex('D');
+  const vertexB = new GraphVertex('B');
+  const vertexC = new GraphVertex('C');
+  const vertexH = new GraphVertex('H');
+
+  const edgeSE = new GraphEdge(vertexS, vertexE, 8);
+  const edgeSA = new GraphEdge(vertexS, vertexA, 10);
+  const edgeED = new GraphEdge(vertexE, vertexD, 1);
+  const edgeDA = new GraphEdge(vertexD, vertexA, -4);
+  const edgeDC = new GraphEdge(vertexD, vertexC, -1);
+  const edgeAC = new GraphEdge(vertexA, vertexC, 2);
+  const edgeCB = new GraphEdge(vertexC, vertexB, -2);
+  const edgeBA = new GraphEdge(vertexB, vertexA, 1);
+
+  const graph = new Graph(true);
+  graph
+    .addVertex(vertexH)
+    .addEdge(edgeSE)
+    .addEdge(edgeSA)
+    .addEdge(edgeED)
+    .addEdge(edgeDA)
+    .addEdge(edgeDC)
+    .addEdge(edgeAC)
+    .addEdge(edgeCB)
+    .addEdge(edgeBA);
+
+  const { distances, previousVertices } = dijkstra(graph, vertexS);
+
+  t.deepEqual(distances, {
+    H: Infinity,
+    S: 0,
+    A: 5,
+    B: 5,
+    C: 7,
+    D: 9,
+    E: 8,
+  });
+
+  t.is(previousVertices.H, null);
+  t.is(previousVertices.S, null);
+  t.is(previousVertices.B.getKey(), 'C');
+  t.is(previousVertices.C.getKey(), 'A');
+  t.is(previousVertices.A.getKey(), 'D');
+  t.is(previousVertices.D.getKey(), 'E');
 
 });
