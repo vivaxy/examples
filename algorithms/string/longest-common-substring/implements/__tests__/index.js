6b8747a20e8c427adbd3d0ccf3b37fc5ba1f4bbd
vivaxy <xyxuye2007@126.com>
2018-07-13 11:00:52 +0800

    feat(algorithms/string/longest-common-substring/implements): :sparkles:

---
+++ algorithms/string/longest-common-substring/implements/__tests__/index.js
@@ -0,0 +1,14 @@
+/**
+ * @since 20180712 19:49
+ * @author vivaxy
+ */
+
+const test = require('ava');
+const longestCommonSubstring = require('../index.js');
+
+test('longest common substring', (t) => {
+
+  t.is(longestCommonSubstring('ABABC', 'BABCA'), 'BABC');
+  t.is(longestCommonSubstring('BABCA', 'ABCBA'), 'ABC');
+
+});
---
+++ algorithms/string/longest-common-substring/implements/index.js
@@ -0,0 +1,50 @@
+/**
+ * @since 20180712 19:48
+ * @author vivaxy
+ */
+
+module.exports = function longestCommonSubstring(s1, s2) {
+
+  const matrix = [];
+
+  let lcsLength = 0;
+  let lcsI = 0;
+  let lcsJ = 0;
+
+  for (let i = 0; i <= s1.length; i++) {
+    matrix[i] = [];
+
+    for (let j = 0; j <= s2.length; j++) {
+      if (i === 0 || j === 0) {
+        matrix[i][j] = 0;
+      } else {
+
+        if (s1[i - 1] === s2[j - 1]) {
+          matrix[i][j] = matrix[i - 1][j - 1] + 1;
+        } else {
+          matrix[i][j] = 0;
+        }
+
+        if (matrix[i][j] > lcsLength) {
+          lcsLength = matrix[i][j];
+          lcsI = i;
+          lcsJ = j;
+        }
+
+      }
+    }
+  }
+
+  if (lcsLength === 0) {
+    return '';
+  }
+
+  let lcs = '';
+  while (matrix[lcsI][lcsJ] > 0) {
+    lcs = s1[lcsI - 1] + lcs;
+    lcsI--;
+    lcsJ--;
+  }
+
+  return lcs;
+};
