c9ac6fe3223b37f83000fdde896189d870caca81
vivaxy <xyxuye2007@126.com>
2018-08-28 11:07:53 +0800

    refactor(qq-maps): :recycle:mark-polygon-area

--- qq-maps/mark-polygon-area/index.js
+++ qq-maps/mark-polygon-area/index.js
@@ -3,40 +3,52 @@
  * @author vivaxy
  */
 
-var zoom = 14;
+function init() {
 
-var latlng = new qq.maps.LatLng(-31.2162311, 121.4203236);
+  var zoom = 14;
 
-var container = document.querySelector('.js-container');
+  var latlng = new qq.maps.LatLng(-31.2162311, 121.4203236);
 
-var map = new qq.maps.Map(container, {
+  var container = document.querySelector('.js-container');
+
+  var map = new qq.maps.Map(container, {
     center: latlng,
     zoom: zoom,
     draggable: false,
     scrollwheel: false,
     disableDoubleClickZoom: true,
     disableDefaultUI: true,
-});
+  });
 
-var searchService = new qq.maps.SearchService({
-    //请求成功回调函数
+  var searchService = new qq.maps.SearchService({
+    // 请求成功回调函数
     complete: function(results) {
-        var pois = results.detail.pois;
-        var infoWin = new qq.maps.InfoWindow({
-            map: map
-        });
-        var latlngBounds = new qq.maps.LatLngBounds();
-        for (var i = 0, l = pois.length; i < l; i++) {
-            var poi = pois[i];
-            //扩展边界范围，用来包含搜索到的Poi点
-            latlngBounds.extend(poi.latLng);
-        }
-        //调整地图视野
-        map.fitBounds(latlngBounds);
+      var pois = results.detail.pois;
+      var infoWin = new qq.maps.InfoWindow({
+        map: map,
+      });
+      var latlngBounds = new qq.maps.LatLngBounds();
+      for (var i = 0, l = pois.length; i < l; i++) {
+        var poi = pois[i];
+        // 扩展边界范围，用来包含搜索到的Poi点
+        latlngBounds.extend(poi.latLng);
+      }
+      // 调整地图视野
+      map.fitBounds(latlngBounds);
     },
     error: function() {
-        alert("出错了，请输入正确的经纬度！！！");
-    }
-});
+      alert('出错了，请输入正确的经纬度！！！');
+    },
+  });
 
-searchService.search('上海');
+  searchService.search('上海');
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '//map.qq.com/api/js?v=2.exp&key=F2PBZ-YRNWD-SUL4C-HSCSP-SVWWJ-YRB4N&callback=init';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
