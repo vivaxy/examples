3dcd7af05daf472b64763aab1ce556d1c3dca61d
vivaxy2012 <xyxuye2007@126.com>
2019-02-25 19:16:30 +0800

    test-update-git-username

--- native-api/3d-touch/index.js
+++ native-api/3d-touch/index.js
@@ -3,30 +3,28 @@
  * @author vivaxy
  */
 
-'use strict';
-
 var body = document.querySelector('body');
 var touchList = document.querySelector('touch-list');
 
-var forEach = function (arrayLike, callback) {
-    return Array.prototype.forEach.call(arrayLike, callback);
+var forEach = function(arrayLike, callback) {
+  return Array.prototype.forEach.call(arrayLike, callback);
 };
 
-var logTouch = function (touch) {
-    var touchLogger = document.createElement('touch');
-    touchLogger.textContent = touch.force;
-    touchList.appendChild(touchLogger);
+var logTouch = function(touch) {
+  var touchLogger = document.createElement('touch');
+  touchLogger.textContent = touch.force;
+  touchList.appendChild(touchLogger);
 };
 
-var clearTouchList = function () {
-    while (touchList.childElementCount) {
-        touchList.removeChild(touchList.firstElementChild);
-    }
-    return touchList;
+var clearTouchList = function() {
+  while (touchList.childElementCount) {
+    touchList.removeChild(touchList.firstElementChild);
+  }
+  return touchList;
 };
 
-body.addEventListener('touchstart', function (e) {
-    clearTouchList();
-    var touches = e.targetTouches;
-    forEach(touches, logTouch);
+body.addEventListener('touchstart', function(e) {
+  clearTouchList();
+  var touches = e.targetTouches;
+  forEach(touches, logTouch);
 });
