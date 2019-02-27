0539fb505e1dca7e6f151c3f69f39333e42d6d46
vivaxy <xyxuye2007@126.com>
2019-02-27 10:14:50 +0800

    refactor(javascript/is-animated-image): :recycle:

--- javascript/is-animated-image/index.js
+++ javascript/is-animated-image/index.js
@@ -3,6 +3,7 @@
  * @author vivaxy
  * @supported android 5
  * @see https://github.com/sindresorhus/file-type
+ * @see https://github.com/qzb/is-animated
  */
 
 var consoleEl = document.querySelector('.js-console');
@@ -13,35 +14,72 @@ input.addEventListener('change', handleChange);
 function handleChange(e) {
   for (var i = 0; i < e.target.files.length; i++) {
     var fileReader = new FileReader();
-    fileReader.addEventListener('load', handleFileReaderLoad);
-    fileReader.file = e.target.files[i];
-    fileReader.readAsArrayBuffer(fileReader.file);
+    var file = e.target.files[i];
+    fileReader.addEventListener('load', handleFileReaderLoad(file));
+    fileReader.readAsArrayBuffer(file);
   }
 }
 
-function handleFileReaderLoad(e) {
-  var arrayBuffer = e.target.result;
+function handleFileReaderLoad(file) {
+  return function(e) {
+    var imageMeta = getImageMeta(e.target.result);
+    if (imageMeta) {
+      return log(imageMeta.ext, imageMeta.mime, file.name, imageMeta.animated ? '✔' : '✖');
+    }
+    return log('N/A', 'N/A', file.name, 'N/A');
+  };
+}
+
+function log(extension, mime, filename, animated) {
+  consoleEl.innerHTML += '<tr><td>' + extension + '</td><td>' + mime + '</td><td>' + filename + '</td><td>' + animated + '</td></tr>';
+}
+
+function getImageMeta(arrayBuffer) {
   var buffer = new Uint8Array(arrayBuffer);
   if (check(buffer, [0xFF, 0xD8, 0xFF])) {
-    return log('jpg', 'image/jpeg', e.target.file.name, '✖');
+    return {
+      ext: 'jpg',
+      mime: 'image/jpeg',
+      animated: false,
+    };
   }
   if (check(buffer, [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])) {
     // apng has `61 63 54 4C` before first `00 00 00 08`
     if (findIndex(buffer, [0x61, 0x63, 0x54, 0x4C]) === -1) {
-      return log('png', 'image/png', e.target.file.name, '✖');
+      return {
+        ext: 'png',
+        mime: 'image/png',
+        animated: false,
+      };
     }
-    return log('apng', 'image/png', e.target.file.name, '✔');
+    return {
+      ext: 'png',
+      mime: 'image/png',
+      animated: true,
+    };
   }
   if (check(buffer, [0x47, 0x49, 0x46])) {
-    return log('gif', 'image/gif', e.target.file.name, '✔');
+    return {
+      ext: 'gif',
+      mime: 'image/gif',
+      animated: true,
+    };
   }
   if (check(buffer, [0x57, 0x45, 0x42, 0x50], 8)) {
     if (findIndex(buffer, [0x41, 0x4E, 0x49, 0x4D]) === -1) {
-      return log('webp', 'image/webp', e.target.file.name, '✖');
+      return {
+        ext: 'webp',
+        mime: 'image/webp',
+        animated: false,
+      };
     }
-    return log('webp', 'image/webp', e.target.file.name, '✔');
+    return {
+      ext: 'webp',
+      mime: 'image/webp',
+      animated: true,
+    };
   }
-  log('N/A', 'N/A', e.target.file.name, 'N/A');
+  return null;
 }
 
 function check(buffer, codes, offset) {
@@ -67,8 +105,4 @@ function findIndex(buffer, codes, from, to) {
     return i;
   }
   return -1;
-}
-
-function log(extension, mime, filename, animated) {
-  consoleEl.innerHTML += '<tr><td>' + extension + '</td><td>' + mime + '</td><td>' + filename + '</td><td>' + animated + '</td></tr>';
 }
