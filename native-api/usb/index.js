9e885c0489f0fffb0b32ec11ea12f51d8a75fce7
vivaxy <xyxuye2007@126.com>
2019-03-13 13:04:30 +0800

    chore:

--- native-api/usb/index.js
+++ native-api/usb/index.js
@@ -4,22 +4,26 @@
  * @see https://www.youtube.com/watch?v=IpfZ8Nj3uiE
  */
 (async() => {
-  const options = {
-    /**
-     * @see http://www.linux-usb.org/usb.ids
-     */
-    filters: [{ vendorId: 0x2a45 }],
-  };
+  try {
+    const options = {
+      /**
+       * @see http://www.linux-usb.org/usb.ids
+       */
+      filters: [{ vendorId: 0x2a45 }],
+    };
 
-  const device = await navigator.usb.requestDevice(options);
-  console.log('device', device);
+    const device = await navigator.usb.requestDevice(options);
+    console.log('device', device);
 
-  await device.open();
+    await device.open();
 
-  await device.selectConfiguration(0);
-  await device.claimInterface(0);
+    await device.selectConfiguration(0);
+    await device.claimInterface(0);
 
-  await device.transferOut(1, 0x1a);
-  const response = await device.transferIn(2, 5);
-  console.log('response', response);
+    await device.transferOut(1, 0x1a);
+    const response = await device.transferIn(2, 5);
+    console.log('response', response);
+  } catch (exception) {
+    console.log('exception', exception);
+  }
 })();
