e3b6f36b012fc080e6429a7f487d3a5ca5003736
vivaxy <xyxuye2007@126.com>
2019-03-13 13:02:54 +0800

    feat(native-api): :sparkles:usb

--- native-api/usb/index.js
+++ native-api/usb/index.js
@@ -5,9 +5,21 @@
  */
 (async() => {
   const options = {
+    /**
+     * @see http://www.linux-usb.org/usb.ids
+     */
     filters: [{ vendorId: 0x2a45 }],
   };
 
   const device = await navigator.usb.requestDevice(options);
   console.log('device', device);
+
+  await device.open();
+
+  await device.selectConfiguration(0);
+  await device.claimInterface(0);
+
+  await device.transferOut(1, 0x1a);
+  const response = await device.transferIn(2, 5);
+  console.log('response', response);
 })();
