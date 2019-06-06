62c785b5d5ac8cfa066c153387616cd46006215d
vivaxy <xyxuye2007@126.com>
2019-06-06 13:17:29 +0800

    chore(javascript): :sparkles:shapes

--- javascript/shapes/index.js
+++ javascript/shapes/index.js
@@ -2,18 +2,17 @@
  * @since 2019-06-06 10:47
  * @author vivaxy
  */
-let a = {x:1, y:2, z:3};
-
+let a = { x: 1, y: 2, z: 3 };
 let b = {};
 b.x = 1;
 b.y = 2;
 b.z = 3;
 
-console.log("a and b have same map:", %HaveSameMap(a, b));
+console.log('a and b have same map:', %HaveSameMap(a, b)); // => false
 
-a = {x:1,y:2,z:3};
-b = {x: 100, y: 200, z: 300};
-console.log("a and b have same map:", %HaveSameMap(a, b));
+a = { x: 1, y: 2, z: 3 };
+b = { x: 100, y: 200, z: 300 };
+console.log('a and b have same map:', %HaveSameMap(a, b)); // => true
 
 a = {};
 a.x = 1;
@@ -24,4 +23,4 @@ b = {};
 b.x = 100;
 b.y = 200;
 b.z = 200;
-console.log("a and b have same map:", %HaveSameMap(a, b));
+console.log('a and b have same map:', %HaveSameMap(a, b)); // => true
