d3a162ac019f0d9cc18b81e5da62a1656481cfcb
vivaxy <xyxuye2007@126.com>
2019-07-04 13:22:43 +0800

    feat(node js): :sparkles: cli size

---
+++ node-js/readline/cli-size.js
@@ -0,0 +1,7 @@
+/**
+ * @since 2019-07-04 13:19
+ * @author vivaxy
+ */
+
+console.log(process.stdout.getWindowSize());
+console.log([process.stdout.columns, process.stdout.rows]);
--- node-js/readline/index.js
+++ node-js/readline/index.js
@@ -2,23 +2,23 @@
  * @since 15-08-03 14:35
  * @author vivaxy
  */
-var readline = require('readline'),
-    rl = readline.createInterface(process.stdin, process.stdout);
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 rl.setPrompt('OHAI> ');
 rl.prompt();
 
-rl.on('line', function (line) {
-    switch (line.trim()) {
-        case 'hello':
-            console.log('world!');
-            break;
-        default:
-            console.log('Say what? I might have heard `' + line.trim() + '`');
-            break;
-    }
-    rl.prompt();
-}).on('close', function () {
-    console.log('Have a great day!');
-    process.exit(0);
+rl.on('line', function(line) {
+  switch (line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
 });
