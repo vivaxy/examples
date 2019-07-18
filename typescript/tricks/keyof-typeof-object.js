78dd111499dfcb20d88af490bde90672a2990d10
vivaxy <xyxuye2007@126.com>
2019-07-18 11:15:21 +0800

    chore(typescript): :sparkles: tricks

--- typescript/tricks/keyof-typeof-object.js
+++ typescript/tricks/keyof-typeof-object.js
@@ -1,13 +1,21 @@
+'use strict';
 var objectOfKeyofTypeofObject = {
-    name: 'name',
-    age: 'age'
+  name: 'name',
+  age: 'age',
 };
-function getValueFromObject(key) {
-    return objectOfKeyofTypeofObject[key];
+function getValueFromObject1(key) {
+  return objectOfKeyofTypeofObject[key];
 }
 function getValueFrom(obj, key) {
-    return obj[key];
+  return obj[key];
 }
 function getValue(obj, key) {
-    return obj[key];
+  return obj[key];
 }
+function log(largeObject) {
+  console.log(largeObject);
+}
+var largeObject = {
+  key: '0',
+};
+log(largeObject);
--- typescript/tricks/keyof-typeof-object.ts
+++ typescript/tricks/keyof-typeof-object.ts
@@ -14,3 +14,19 @@ function getValueFrom<T>(obj: T, key: keyof T) {
 function getValue<T, K extends keyof T>(obj: T, key: K) {
   return obj[key];
 }
+
+// error
+interface SomeObject {
+  key: string;
+}
+
+function log(input: SomeObject) {
+  console.log(input);
+}
+
+// any by passing function parameter type check, why?
+const someObject: any = {
+  key: 0,
+};
+
+log(someObject);
