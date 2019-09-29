4e2c3aa714d2857bf54a0dc31a373cf7253e5b96
vivaxy <xyxuye2007@126.com>
2019-09-29 23:23:13 +0800

    test(algorithms/string/knuth-morris-pratt): :white_check_mark:

--- algorithms/string/knuth-morris-pratt/implements/__tests__/index.js
+++ algorithms/string/knuth-morris-pratt/implements/__tests__/index.js
@@ -7,13 +7,18 @@ const test = require('ava');
 const knuthMorrisPratt = require('../index.js');
 
 test('Knuth Morris Pratt', (t) => {
-  t.is(knuthMorrisPratt('', ''), 0);
-  t.is(knuthMorrisPratt('a', ''), 0);
-  t.is(knuthMorrisPratt('a', 'a'), 0);
-  t.is(knuthMorrisPratt('abcbcglx', 'abca'), -1);
-  t.is(knuthMorrisPratt('abcbcglx', 'bcgl'), 3);
-  t.is(knuthMorrisPratt('abcxabcdabxabcdabcdabcy', 'abcdabcy'), 15);
-  t.is(knuthMorrisPratt('abcxabcdabxabcdabcdabcy', 'abcdabca'), -1);
-  t.is(knuthMorrisPratt('abcxabcdabxaabcdabcabcdabcdabcy', 'abcdabca'), 12);
-  t.is(knuthMorrisPratt('abcxabcdabxaabaabaaaabcdabcdabcy', 'aabaabaaa'), 11);
+  [
+    ['', ''],
+    ['a', ''],
+    ['a', 'a'],
+    ['abcbcglx', 'abca'],
+    ['abcbcglx', 'bcgl'],
+    ['abcxabcdabxabcdabcdabcy', 'abcdabcy'],
+    ['abcxabcdabxabcdabcdabcy', 'abcdabca'],
+    ['abcxabcdabxaabcdabcabcdabcdabcy', 'abcdabca'],
+    ['abcxabcdabxaabaabaaaabcdabcdabcy', 'aabaabaaa'],
+    ['bacabdabcdabcab', 'abcab'],
+  ].forEach(function([text, pattern]) {
+    t.is(knuthMorrisPratt(text, pattern), text.indexOf(pattern));
+  });
 });
