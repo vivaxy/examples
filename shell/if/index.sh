f62fcc7a7512d5cf33251b49efec2389f86b8bcf
vivaxy <xyxuye2007@126.com>
2020-01-15 11:01:26 +0800

    fix(shell): :bug: if

--- shell/if/index.sh
+++ shell/if/index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @since 2020-01-15 10:46
 # @author vivaxy
-if [ "${NODE_ENV}" == "" ]
+if [[ "${NODE_ENV}" == "" ]]
 then
   echo "Please Supply NODE_ENV: NODE_ENV=production sh ./index.sh"
 else
