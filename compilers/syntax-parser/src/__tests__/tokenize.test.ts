d7c97e766b5f0095175d2d541fec9ecb4926a130
vivaxy <xyxuye2007@126.com>
2020-02-26 19:55:12 +0800

    feat(compilers/syntax-parser): :sparkles:  implement tokenize

--- compilers/syntax-parser/src/__tests__/tokenize.test.ts
+++ compilers/syntax-parser/src/__tests__/tokenize.test.ts
@@ -4,10 +4,40 @@
  */
 import tokenize from '../tokenize';
 
-test('', function () {
-  const stateMachine = {};
-  const code = ``;
-  const tokens = tokenize(stateMachine, code);
-  const expected = [];
-  expect(tokens).toBe(expected);
-})
+test('binary operation', function() {
+  const patterns = [
+    {
+      type: 'Number',
+      regExps: [/[0-9]+/],
+    },
+    {
+      type: 'Operator',
+      regExps: [/[\+|-]/],
+    },
+    {
+      type: 'WhiteSpace',
+      regExps: [/\s+/],
+      ignore: true,
+    },
+  ];
+  const code = `1 + 2`;
+  const tokens = tokenize(patterns, code);
+  const expected = [
+    {
+      type: 'Number',
+      value: '1',
+      position: [0, 1],
+    },
+    {
+      type: 'Operator',
+      value: '+',
+      position: [2, 3],
+    },
+    {
+      type: 'Number',
+      value: '2',
+      position: [4, 5],
+    },
+  ];
+  expect(tokens).toStrictEqual(expected);
+});
--- compilers/syntax-parser/src/tokenize.ts
+++ compilers/syntax-parser/src/tokenize.ts
@@ -2,7 +2,44 @@
  * @since 2020-02-26 02:39
  * @author vivaxy
  */
-type TokenStateMachine = {
-  state: string,
-  
-};
+interface Pattern {
+  type: string;
+  regExps: RegExp[];
+  ignore?: boolean;
+}
+
+function getMatched(patterns: Pattern[], code: string) {
+  for (const pattern of patterns) {
+    for (const regExp of pattern.regExps) {
+      const formattedRegExp = new RegExp(`^(${regExp.source})`);
+      const matched = code.match(formattedRegExp);
+      if (matched) {
+        return {
+          type: pattern.type,
+          regExp: regExp,
+          ignore: pattern.ignore,
+          value: matched[1],
+        };
+      }
+    }
+  }
+  throw new Error('Unexpected token');
+}
+
+export default function tokenize(patterns: Pattern[], code: string) {
+  let index = 0;
+  const tokens = [];
+  while (index < code.length) {
+    const matched = getMatched(patterns, code.slice(index));
+    if (matched.ignore) {
+      index += matched.value.length;
+    } else {
+      tokens.push({
+        type: matched.type,
+        value: matched.value,
+        position: [index, (index += matched.value.length)],
+      });
+    }
+  }
+  return tokens;
+}
