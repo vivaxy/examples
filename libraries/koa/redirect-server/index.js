153227f940e3a7568ca4a884e6de421e56bdd1a3
vivaxy <xyxuye2007@126.com>
2020-05-13 19:30:27 +0800

    feat(koa/redirect-server): :sparkles:

--- libraries/koa/redirect-server/index.js
+++ libraries/koa/redirect-server/index.js
@@ -7,6 +7,10 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async function(ctx) {
+  if (ctx.path === '/index.html') {
+    ctx.body = `<html><head>Redirect Server</head><body><script src="/redirect.js"></script></body></html>`;
+    return;
+  }
   const redirectURL = 'dianping://user';
   ctx.body = redirectURL;
   if (ctx.headers['user-agent'].includes('dianping')) {
