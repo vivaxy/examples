47c7ff4dce8c81e4301e92fe760d32717efb5888
vivaxy <xyxuye2007@126.com>
2021-04-06 20:17:04 +0800

    fix: :bug: fix none-editable-table-breaks-user-selection-on-android

---
+++ libraries/prosemirror/none-editable-table-breaks-user-selection-on-android/build/_snowpack/pkg/prosemirror-model.js
@@ -0,0 +1,1 @@
+export { D as DOMParser, S as Schema } from './common/index.es-76b61ec3.js';
---
+++ libraries/prosemirror/none-editable-table-breaks-user-selection-on-android/build/_snowpack/pkg/prosemirror-state.js
@@ -0,0 +1,2 @@
+export { E as EditorState } from './common/index.es-41c2ff6a.js';
+import './common/index.es-76b61ec3.js';
