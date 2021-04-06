ed9d03b926eef43b63e686b507371581af9439fb
vivaxy <xyxuye2007@126.com>
2021-04-06 15:15:58 +0800

    fix: :bug: fix selection manipulation

--- native-api/selection/selection-manipulation/index.js
+++ native-api/selection/selection-manipulation/index.js
@@ -2,10 +2,19 @@
  * @since 2021-04-01 16:54
  * @author vivaxy
  */
+function getIndex(node) {
+  const SEARCH_TEXT = 'JavaScript';
+  const start = node.textContent.indexOf(SEARCH_TEXT);
+  const end = start + SEARCH_TEXT.length;
+  return [start, end];
+}
 document.addEventListener('selectionchange', function () {
   setTimeout(function () {
-    const focusNode = document.querySelector('p');
+    const focusNode = document.querySelector('#paragraph');
     const anchorNode = focusNode.childNodes[0];
-    window.getSelection().setBaseAndExtent(anchorNode, 1, anchorNode, 2);
+    const [anchorOffset, focusOffset] = getIndex(anchorNode);
+    window
+      .getSelection()
+      .setBaseAndExtent(anchorNode, anchorOffset, anchorNode, focusOffset);
   }, 3000);
 });
