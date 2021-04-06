a897ad170198104c229bbe79c9c24b4e480720cd
vivaxy <xyxuye2007@126.com>
2021-04-06 13:07:35 +0800

    chore: :sparkles: update the selection within table demo

--- native-api/selection/selection-within-table/index.js
+++ native-api/selection/selection-within-table/index.js
@@ -6,3 +6,16 @@ document.addEventListener('selectionchange', function () {
   const sel = window.getSelection();
   console.log(sel.anchorNode, sel.anchorOffset, sel.focusNode, sel.focusOffset);
 });
+
+document
+  .querySelector('#select-into-not-contentediable-nodes')
+  .addEventListener('click', function () {
+    window
+      .getSelection()
+      .setBaseAndExtent(
+        document.querySelector('#content-editable').childNodes[0],
+        1,
+        document.querySelector('#content-not-editable').childNodes[0],
+        2,
+      );
+  });
