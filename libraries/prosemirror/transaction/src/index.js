7211f6cff9f9dbeed1e4f5a810d71885fb257a3c
vivaxy <xyxuye2007@126.com>
2021-04-12 14:55:34 +0800

    feat: :sparkles: prosemirror-transform

--- libraries/prosemirror/transaction/src/index.js
+++ libraries/prosemirror/transaction/src/index.js
@@ -5,13 +5,15 @@
 import { schema } from 'prosemirror-schema-basic';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
+import { ReplaceStep } from 'prosemirror-transform';
+import { Slice } from 'prosemirror-model';
 
 const UPDATED_SEL = 1,
   UPDATED_MARKS = 2,
   UPDATED_SCROLL = 4;
 
-let state = EditorState.create({ schema });
-let view = new EditorView(document.getElementById('editor'), {
+const state = EditorState.create({ schema });
+const view = new EditorView(document.getElementById('editor'), {
   state,
   dispatchTransaction(transaction) {
     if ((transaction.updated & UPDATED_SEL) > 0) {
@@ -28,4 +30,16 @@ let view = new EditorView(document.getElementById('editor'), {
     let newState = view.state.apply(transaction);
     view.updateState(newState);
   },
+});
+
+document.querySelector('#replace').addEventListener('click', function () {
+  const doc = view.state.doc.toString();
+  console.log('doc', doc);
+  const step = new ReplaceStep(3, 5, Slice.empty);
+  const result = step.apply(view.state.doc);
+  if (result.failed) {
+    console.log('failed', result.failed);
+  } else {
+    console.log(result.doc.toString());
+  }
 });
