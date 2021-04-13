9afec7734b1b404de5973256e2ffc78f969a9423
vivaxy <xyxuye2007@126.com>
2021-04-13 13:39:38 +0800

    feat: :sparkles: add prosemirror mark samples

--- libraries/prosemirror/marks/src/index.js
+++ libraries/prosemirror/marks/src/index.js
@@ -11,7 +11,7 @@ import { exampleSetup } from 'prosemirror-example-setup';
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
-const mySchema = new Schema({
+const sampleSchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
   marks: {
     strong: {
@@ -21,7 +21,7 @@ const mySchema = new Schema({
       parseDOM: [
         {
           tag: 'strong',
-          getAttrs: function (node) {
+          getAttrs(node) {
             return {
               'data-id': node.getAttribute('data-id'),
             };
@@ -35,14 +35,57 @@ const mySchema = new Schema({
         return ['strong', attrs, 0];
       },
     },
+    background: {
+      excludes: '',
+      attrs: {
+        'data-background-id': { default: 0 },
+      },
+      parseDOM: [
+        {
+          tag: '[data-background-id]',
+          getAttrs(node) {
+            return {
+              'data-background-id': node.getAttribute('data-background-id'),
+            };
+          },
+        },
+      ],
+      toDOM(node) {
+        const attrs = {
+          'data-background-id': node.attrs['data-background-id'] || Date.now(),
+          style: 'background-color: rgba(255, 255, 0, 0.1)',
+        };
+        return ['span', attrs, 0];
+      },
+    },
   },
 });
 
 const view = new EditorView(document.querySelector('#editor'), {
   state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(
+    doc: DOMParser.fromSchema(sampleSchema).parse(
       document.querySelector('#content'),
     ),
-    plugins: exampleSetup({ schema: mySchema }),
+    plugins: exampleSetup({ schema: sampleSchema }),
   }),
+});
+
+const $markStart = document.querySelector('#mark-start');
+const $markEnd = document.querySelector('#mark-end');
+
+document.querySelector('#add-mark').addEventListener('click', function () {
+  const from = $markStart.value;
+  const to = $markEnd.value;
+  const mark = sampleSchema.marks.background.create({
+    'data-background-id': 'b-' + Date.now(),
+  });
+  view.dispatch(view.state.tr.addMark(from, to, mark));
+});
+
+document.querySelector('#remove-mark').addEventListener('click', function () {
+  const from = $markStart.value;
+  const to = $markEnd.value;
+  view.dispatch(
+    view.state.tr.removeMark(from, to, sampleSchema.marks.background),
+  );
 });
