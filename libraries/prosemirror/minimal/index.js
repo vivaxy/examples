db26d0063852409555a4c1cecb362641bbb38f8d
vivaxy <xyxuye2007@126.com>
2021-04-15 17:06:10 +0800

    chore: :recycle: proemirror minimal

--- libraries/prosemirror/minimal/index.js
+++ libraries/prosemirror/minimal/index.js
@@ -5,8 +5,12 @@
 import { schema } from 'https://cdn.skypack.dev/prosemirror-schema-basic';
 import { EditorState } from 'https://cdn.skypack.dev/prosemirror-state';
 import { EditorView } from 'https://cdn.skypack.dev/prosemirror-view';
+import { DOMParser } from 'https://cdn.skypack.dev/prosemirror-model';
 
-const state = EditorState.create({ schema });
-const view = new EditorView(document.getElementById('editor'), {
+const state = EditorState.create({
+  schema,
+  doc: DOMParser.fromSchema(schema).parse(document.querySelector('#content')),
+});
+const view = new EditorView(document.querySelector('#editor'), {
   state,
 });
