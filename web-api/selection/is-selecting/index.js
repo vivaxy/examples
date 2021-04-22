e9dd3b64fd438fa366234310a70cf6c8252c5549
vivaxy <xyxuye2007@126.com>
2021-04-22 20:50:51 +0800

    feat: :sparkles:  add mouse event to the is selecting

--- web-api/selection/is-selecting/index.js
+++ web-api/selection/is-selecting/index.js
@@ -2,9 +2,15 @@
  * @since 2021-04-06 20:28
  * @author vivaxy
  */
-['click', 'touchstart', 'touchmove', 'touchend', 'touchcancel'].map(function (
-  event,
-) {
+[
+  'click',
+  'touchstart',
+  'touchmove',
+  'touchend',
+  'touchcancel',
+  'mousedown',
+  'mouseup',
+].map(function (event) {
   document.addEventListener(event, function (e) {
     console.log(event, e);
   });
