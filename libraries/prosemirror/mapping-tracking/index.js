d7d94ea02d9d2c1bdd7ed6c6b6e96df310c3724a
vivaxy <xyxuye2007@126.com>
2021-05-27 15:53:57 +0800

    feat(prosemirror): update mapping-tracking sample, add serialize mapping

--- libraries/prosemirror/mapping-tracking/index.js
+++ libraries/prosemirror/mapping-tracking/index.js
@@ -6,7 +6,7 @@ import { schema } from 'prosemirror-schema-basic';
 import { EditorState, TextSelection } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { DOMParser } from 'prosemirror-model';
-import { Mapping } from 'prosemirror-transform';
+import { Mapping, StepMap } from 'prosemirror-transform';
 
 const $content = document.querySelector('#content');
 const $editor = document.querySelector('#editor');
@@ -16,19 +16,24 @@ const originalState = EditorState.create({
   doc: DOMParser.fromSchema(schema).parse($content),
 });
 
-const mapping = new Mapping();
+let mappings = [];
 
 const view = new EditorView($editor, {
   state: originalState,
   dispatchTransaction(transaction) {
     const newState = view.state.apply(transaction);
     if (transaction.docChanged) {
-      mapping.appendMapping(transaction.mapping);
-      console.log('mapping', mapping);
+      const _mappings = transaction.mapping.maps.map(function (step) {
+        return step.ranges;
+      });
+      mappings.push(..._mappings);
+      console.log('mappings', mappings);
     }
     view.updateState(newState);
   },
 });
+
+const BASE_OFFSET = 1;
 
 document.querySelector('#sync').addEventListener('click', function () {
   const selection = window.getSelection();
@@ -45,10 +50,16 @@ document.querySelector('#sync').addEventListener('click', function () {
     return;
   }
 
-  let from = startOffset + 1;
-  let to = endOffset + 1;
+  let from = startOffset + BASE_OFFSET;
+  let to = endOffset + BASE_OFFSET;
+  const mapping = new Mapping(
+    mappings.map(function (_mapping) {
+      return new StepMap(_mapping);
+    }),
+  );
   from = mapping.map(from);
-  to = mapping.map(to);
+  // -1 means select ahead
+  to = mapping.map(to, -1);
 
   const sel = TextSelection.create(view.state.doc, from, to);
   const tr = view.state.tr;
