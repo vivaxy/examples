fc8304685de81efa4ac7c7f689552f9e795eaf2f
vivaxy <xyxuye2007@126.com>
2021-07-07 18:21:10 +0800

    feat(yjs): add snapshot

--- libraries/yjs/basic/index.js
+++ libraries/yjs/basic/index.js
@@ -75,8 +75,29 @@ function relativePositionAndAbsolutePosition() {
   console.log('absPos', absPos);
 }
 
+function snapshot() {
+  const docA = new Y.Doc();
+  const textA = docA.getText(TEXT_KEY);
+  textA.insert(0, 'ABC');
+  const docB = new Y.Doc();
+  const textB = docB.getText(TEXT_KEY);
+  Y.applyUpdate(docB, Y.encodeStateAsUpdate(docA));
+  const snapshot1 = Y.snapshot(docB);
+  textB.delete(1, 1);
+  const snapshot2 = Y.snapshot(docB);
+  textB.insert(1, 'T');
+  const snapshot3 = Y.snapshot(docB);
+  console.log(
+    'snapshot with deleteSet and stateMap',
+    snapshot1,
+    snapshot2,
+    snapshot3,
+  );
+}
+
 updateText();
 syncDocs();
 updateYXmlFragment();
 mergeConflict();
 relativePositionAndAbsolutePosition();
+snapshot();
