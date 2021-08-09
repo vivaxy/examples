6b7c53a652c16e38c5c4072f21260e78ec9876d5
vivaxy <xyxuye2007@126.com>
2021-08-09 13:11:17 +0800

    chore(yjs): add update size tests

--- libraries/yjs/update-size/index.js
+++ libraries/yjs/update-size/index.js
@@ -4,6 +4,12 @@
  */
 import * as Y from 'yjs';
 
+function sleep(timeout) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, timeout);
+  });
+}
+
 function getByteLengthForUpdate(arrayBuffer) {
   return arrayBuffer.byteLength;
 }
@@ -16,51 +22,72 @@ function getByteLengthForText(text) {
 
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function sameClientAddText(gc = false) {
+async function sameClientAddText(pud = false, gc = false) {
   const doc = new Y.Doc();
   doc.gc = gc;
+
+  if (pud) {
+    const permanentUserData = new Y.PermanentUserData(doc);
+    permanentUserData.setUserMapping(doc, doc.clientID, 'test-user-name');
+  }
+
   const xmlFragment = doc.getXmlFragment('html');
   const xmlText = new Y.XmlText();
   xmlFragment.insert(0, [xmlText]);
 
   const data = [];
-  let textSize = 0;
+  let textBytes = 0;
 
   for (let i = 0; i < 11; i++) {
     const update = Y.encodeStateAsUpdate(doc);
-    const updateSize = getByteLengthForUpdate(update);
-    data.push({ textSize, updateSize });
+    const updateBytes = getByteLengthForUpdate(update);
+    data.push({ textBytes, updateBytes });
 
     const text = chars.repeat(100);
     xmlText.insert(0, text);
-    textSize += getByteLengthForText(text);
+    if (pud) {
+      await sleep();
+    }
+    textBytes += getByteLengthForText(text);
   }
 
   console.table(data);
   return doc;
 }
 
-function sameClientDelete(gc = false, seq = false) {
+async function sameClientDelete(seq = false, pud = false, gc = false) {
   const doc = new Y.Doc();
   doc.gc = gc;
-  doc.clientId = 0;
+
+  if (pud) {
+    const permanentUserData = new Y.PermanentUserData(doc);
+    permanentUserData.setUserMapping(doc, doc.clientID, 'test-user-name');
+  }
+
   const xmlFragment = doc.getXmlFragment('html');
   const xmlText = new Y.XmlText();
   xmlFragment.insert(0, [xmlText]);
   const text = chars.repeat(100);
   xmlText.insert(0, text);
+  if (pud) {
+    await sleep();
+  }
 
   const data = [];
-  let deleteSize = 0;
+  let deletedBytes = 0;
 
   for (let i = 0; i < 11; i++) {
     const update = Y.encodeStateAsUpdate(doc);
-    const updateSize = getByteLengthForUpdate(update);
-    data.push({ deleteSize, updateSize });
+    const updateBytes = getByteLengthForUpdate(update);
+    data.push({ deletedBytes, updateBytes });
 
-    for (let j = 0; j < 100; j++) {
-      xmlText.delete(seq ? 0 : 1, 1);
-      deleteSize += getByteLengthForText(text[0]);
+    const deleteSteps = 100;
+    for (let j = 0; j < deleteSteps; j++) {
+      xmlText.delete(seq ? 0 : i * deleteSteps + j, 1);
+      deletedBytes += getByteLengthForText(text[0]);
+    }
+    if (pud) {
+      await sleep();
     }
   }
 
@@ -68,44 +95,95 @@ function sameClientDelete(gc = false, seq = false) {
   return doc;
 }
 
-function clientsEdit(gc = false) {
+async function clientsEdit(pud = false, gc = false) {
   const serverDoc = new Y.Doc();
   serverDoc.gc = gc;
+
   const xmlFragment = serverDoc.getXmlFragment('html');
   const xmlText = new Y.XmlText();
   xmlFragment.insert(0, [xmlText]);
 
   const data = [];
-  let clientCount = 0;
-  let textSize = 0;
+  let clientsCount = 0;
+  let textBytes = 0;
   for (let i = 0; i < 11; i++) {
-    const updateSize = getByteLengthForUpdate(Y.encodeStateAsUpdate(serverDoc));
+    const updateBytes = getByteLengthForUpdate(
+      Y.encodeStateAsUpdate(serverDoc),
+    );
     data.push({
-      clientCount,
-      updateSize,
-      textSize,
-      otherSize: updateSize - textSize,
+      clientsCount,
+      updateBytes,
+      textBytes,
+      otherBytes: updateBytes - textBytes,
     });
 
     for (let j = 0; j < 10; j++) {
       const clientDoc = new Y.Doc();
+
+      if (pud) {
+        const permanentUserData = new Y.PermanentUserData(clientDoc);
+        permanentUserData.setUserMapping(
+          clientDoc,
+          clientDoc.clientID,
+          'test-user-name',
+        );
+      }
+
       Y.applyUpdate(clientDoc, Y.encodeStateAsUpdate(serverDoc));
       const xmlFragment = clientDoc.getXmlFragment('html');
       const xmlText = xmlFragment.get(0);
       xmlText.insert(0, chars);
-      textSize += getByteLengthForText(chars);
+
+      if (pud) {
+        await sleep();
+      }
+
+      textBytes += getByteLengthForText(chars);
       const update = Y.encodeStateAsUpdate(clientDoc);
       Y.applyUpdate(serverDoc, update);
-      clientCount++;
+      clientsCount++;
     }
   }
+
   console.table(data);
 }
 
-sameClientAddText(true);
-sameClientAddText(false);
-sameClientDelete(true, true);
-sameClientDelete(false, true);
-sameClientDelete(true, false);
-sameClientDelete(false, false);
-clientsEdit();
+const tests = {
+  async 'textBytes -> updateBytes (w & w/o gc)'() {
+    await sameClientAddText(false, false);
+    await sameClientAddText(false, true);
+  },
+  async 'textBytes -> updateBytes(w & w/o pud)'() {
+    await sameClientAddText(false);
+    await sameClientAddText(true);
+  },
+  async 'deletedBytes -> updateBytes (w & w/o gc)'() {
+    await sameClientDelete(true, false);
+    await sameClientDelete(true, true);
+  },
+  async 'deletedBytes -> updateBytes (w & w/o sequence deletion)'() {
+    await sameClientDelete(true);
+    await sameClientDelete(false);
+  },
+  async 'clientsCount -> updateBytes'() {
+    await clientsEdit();
+  },
+  async 'clientsCount -> updateBytes (w & w/o pud)'() {
+    await clientsEdit(false);
+    await clientsEdit(true);
+  },
+  async 'deletedBytes -> updateBytes (w & w/o pud)'() {
+    await sameClientDelete(false, false);
+    await sameClientDelete(false, true);
+  },
+};
+
+Object.keys(tests).forEach(function (name) {
+  const button = document.createElement('button');
+  button.textContent = name;
+  button.addEventListener('click', tests[name]);
+  document.body.appendChild(button);
+
+  const br = document.createElement('br');
+  document.body.appendChild(br);
+});
