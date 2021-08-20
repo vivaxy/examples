c5a3fb64156926014a100970c44752f237bc8562
vivaxy <xyxuye2007@126.com>
2021-08-20 20:33:49 +0800

    feat(clipboard): implement a better clipboard

--- web-api/clipboard/index.js
+++ web-api/clipboard/index.js
@@ -2,9 +2,10 @@
  * @since 2019-02-03 13:51
  * @author vivaxy
  */
-
 const input = document.querySelector('#input');
 const logger = document.querySelector('#logger');
+const $showHTMLAsHTML = document.querySelector('#html-as-html');
+const $showHTMLAsText = document.querySelector('#html-as-text');
 document.querySelector('#read').addEventListener('click', readClipboard);
 document
   .querySelector('#readText')
@@ -13,83 +14,168 @@ document.querySelector('#write').addEventListener('click', writeClipboard);
 document
   .querySelector('#writeText')
   .addEventListener('click', writeClipboardText);
-['cut', 'copy', 'paste'].forEach((eventName) => {
+['cut', 'copy', 'paste', 'drop', 'drag'].forEach((eventName) => {
   document.addEventListener(eventName, onEvent(eventName));
 });
 
-function readClipboard() {
-  navigator.permissions.query({ name: 'clipboard-read' }).then((result) => {
-    if (result.state === 'granted' || result.state === 'prompt') {
-      navigator.clipboard.read().then((data) => {
-        for (let i = 0; i < data.items.length; i++) {
-          if (data.items[i].type !== 'text/plain') {
-            log('Clipboard contains non-text data. Unable to access it.');
-          } else {
-            input.value = data.items[i].getAs('text/plain');
-          }
+async function readImage(blob) {
+  const objectURL = URL.createObjectURL(blob);
+  const $img = document.createElement('img');
+  $img.style.maxHeight = '200px';
+  $img.src = objectURL;
+  return $img;
+}
+
+const clipboardItemBlobReaders = {
+  async 'text/plain'(blob) {
+    return await blob.text();
+  },
+  async 'text/html'(blob) {
+    return await blob.text();
+  },
+  'image/png': readImage,
+  'image/jpg': readImage,
+};
+
+const dataTransferItemReaders = {
+  string(dataTransferItem) {
+    return new Promise(function (resolve) {
+      dataTransferItem.getAsString(resolve);
+    });
+  },
+  async file(dataTransferItem) {
+    const file = dataTransferItem.getAsFile();
+    return await readImage(file);
+  },
+};
+
+async function checkClipboardPermission(permissionName) {
+  const clipboardReadPermission = await navigator.permissions.query({
+    name: permissionName,
+  });
+  if (
+    clipboardReadPermission.state === 'granted' ||
+    clipboardReadPermission.state === 'prompt'
+  ) {
+    // OK
+  } else {
+    throw new Error(result.state);
+  }
+}
+
+async function readClipboard() {
+  try {
+    clearLog();
+    await checkClipboardPermission('clipboard-read');
+
+    const clipboardItems = await navigator.clipboard.read();
+    for (const clipboardItem of clipboardItems) {
+      for (const type of clipboardItem.types) {
+        const blob = await clipboardItem.getType(type);
+        const reader = clipboardItemBlobReaders[type];
+        if (!reader) {
+          log(type, 'Unhandled type');
+          continue;
         }
-      });
-    } else {
-      log('clipboard-read ' + result.state);
+        const content = await reader(blob);
+        if (type === 'text/plain') {
+          input.value = content;
+        }
+        log(content, type);
+      }
     }
-  });
+  } catch (e) {
+    log(e.message, e.name);
+  }
 }
 
-function readClipboardText() {
-  navigator.clipboard.readText().then((clipText) => {
-    input.value = clipText;
-  });
+async function readClipboardText() {
+  try {
+    clearLog();
+    await checkClipboardPermission('clipboard-read');
+    const content = await navigator.clipboard.readText();
+    input.value = content;
+    log(content);
+  } catch (e) {
+    log(e.message, e.name);
+  }
 }
 
-function writeClipboard() {
-  navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
-    if (result.state === 'granted' || result.state === 'prompt') {
-      const data = new DataTransfer();
-      data.items.add('text/plain', input.value);
-      navigator.clipboard.write(data).then(() => {
-        log('write clipboard OK');
-      });
-    } else {
-      log('clipboard-write ' + result.state);
-    }
-  });
+async function writeClipboard() {
+  try {
+    clearLog();
+    await checkClipboardPermission('clipboard-write');
+    const blob = new Blob([input.value], { type: 'text/plain' });
+    const clipboardItem = new ClipboardItem({
+      [blob.type]: blob,
+    });
+    await navigator.clipboard.write([clipboardItem]);
+    log('OK');
+  } catch (e) {
+    log(e.message, e.name);
+  }
 }
 
-function writeClipboardText() {
-  navigator.clipboard.writeText(input.value).then(() => {
-    log('write clipboard OK');
-  });
+async function writeClipboardText() {
+  try {
+    clearLog();
+    await checkClipboardPermission('clipboard-write');
+    await navigator.clipboard.writeText(input.value);
+    log('OK');
+  } catch (e) {
+    log(e.message, e.name);
+  }
 }
 
 function onEvent(eventName) {
-  return (e) => {
-    log(
-      'event: ' +
-        eventName +
-        '; type: ' +
-        e.clipboardData.type +
-        '; data: ' +
-        e.clipboardData.getData('text/plain'),
-    );
-
-    if (eventName === 'paste') {
-      pasteImage(e);
+  return async function (e) {
+    e.preventDefault();
+    clearLog();
+    const dataTransfer = e.clipboardData || e.dataTransfer;
+    if (!dataTransfer.items.length) {
+      log('', `[${eventName}]`);
     }
+    // cannot read dataTransfer in async functions
+    await Promise.all(
+      Array.from(dataTransfer.items).map(async function (dataTransferItem) {
+        const { kind, type } = dataTransferItem;
+        const reader = dataTransferItemReaders[kind];
+        if (!reader) {
+          log(kind, `[${eventName}] Unhandled kind`);
+          return;
+        }
+        const content = await reader(dataTransferItem);
+        log(content, `[${eventName}] ${type}`);
+      }),
+    );
   };
 }
 
-function log(c) {
-  logger.innerHTML += c + '<br>';
+function clearLog() {
+  logger.innerHTML = '';
 }
 
-function pasteImage(e) {
-  const { items } = e.clipboardData || window.clipboardData;
-  Array.from(items).forEach(function (item) {
-    if (item.type.indexOf('image') !== -1) {
-      const file = item.getAsFile();
-      const img = document.createElement('img');
-      img.src = URL.createObjectURL(file);
-      document.body.appendChild(img);
+function log(content, tag) {
+  const $log = document.createElement('div');
+  $log.style.fontFamily = 'monospace';
+  if (tag) {
+    const $tag = document.createElement('span');
+    $tag.style.color = '#999';
+    $tag.style.marginRight = '8px';
+    $tag.textContent = tag;
+    $log.appendChild($tag);
+  }
+  if (content instanceof HTMLElement) {
+    $log.appendChild(content);
+  } else {
+    const $content = document.createElement('span');
+    if ($showHTMLAsHTML.checked) {
+      $content.innerHTML = content;
+    } else {
+      $content.innerText = content;
     }
-  });
+    $log.appendChild($content);
+  }
+
+  logger.appendChild($log);
 }
