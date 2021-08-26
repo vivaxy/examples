d6ff84644ba5d34508a080e08bd35d63ada5ee91
vivaxy <xyxuye2007@126.com>
2021-08-26 15:35:19 +0800

    chore: update yjs sample

--- libraries/yjs/xml-text/index.js
+++ libraries/yjs/xml-text/index.js
@@ -3,21 +3,37 @@
  * @author vivaxy
  */
 import * as Y from 'yjs';
+import { toJSON } from '../data-visualization/src/data-viewer';
 
 const TEXT_KEY = 'text-key';
 const XML_TEXT_KEY = 'xml-text-key';
 
-const doc = new Y.Doc();
+function differenceBetweenTextAndXmlText() {
+  function applyDelta(type) {
+    type.insert(0, 'ABC');
+    type.applyDelta([{ retain: 1 }, { retain: 1, attributes: { bold: {} } }]);
+  }
 
-function applyDelta(type) {
-  type.insert(0, 'ABC');
-  type.applyDelta([{ retain: 1 }, { retain: 1, attributes: { bold: {} } }]);
+  const doc = new Y.Doc();
+  const text = doc.getText(TEXT_KEY);
+  applyDelta(text);
+  console.log('text', text.toString());
+
+  const xmlText = doc.get(XML_TEXT_KEY, Y.XmlText);
+  applyDelta(xmlText);
+  console.log('xmlText', xmlText.toString());
 }
 
-const text = doc.getText(TEXT_KEY);
-applyDelta(text);
-console.log('text', text.toString());
+function applyWithSameAttributes() {
+  const doc = new Y.Doc();
+  const xmlText = doc.getText(XML_TEXT_KEY);
+  xmlText.insert(0, 'AB');
+  xmlText.applyDelta([
+    { retain: 1, attributes: { bold: true } },
+    { retain: 1, attributes: { bold: true } },
+  ]);
+  console.log('applyWithSameAttributes', toJSON(xmlText, Y));
+}
 
-const xmlText = doc.get(XML_TEXT_KEY, Y.XmlText);
-applyDelta(xmlText);
-console.log('xmlText', xmlText.toString());
+differenceBetweenTextAndXmlText();
+applyWithSameAttributes();
