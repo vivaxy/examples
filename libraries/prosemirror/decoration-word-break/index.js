2c5282c3dc245d40fff401679184c94fda7bc716
vivaxy <xyxuye2007@126.com>
2021-12-23 21:10:16 +0800

    feat(prosemirror): update decoration work break demo

--- libraries/prosemirror/decoration-word-break/index.js
+++ libraries/prosemirror/decoration-word-break/index.js
@@ -11,18 +11,38 @@ const decorationsPlugin = new Plugin({
   state: {
     init(_, { doc }) {
       return DecorationSet.create(doc, [
-        Decoration.inline(189, 190, {
-          style: 'background: rgb(100, 255, 255)',
-        }),
-        Decoration.widget(190, function () {
-          const $cursor = document.createElement('span');
-          $cursor.classList.add('cursor');
-          const $user = document.createElement('span');
-          $user.classList.add('user');
-          $user.textContent = 'vivaxy';
-          $cursor.appendChild($user);
-          return $cursor;
-        }),
+        Decoration.inline(
+          189,
+          190,
+          {
+            style: 'background: rgb(100, 255, 255)',
+          },
+          {
+            type: 'inline',
+            user: '1111',
+            color: 'rgb(100, 255, 255)',
+          },
+        ),
+        Decoration.widget(
+          190,
+          function () {
+            const $root = document.createElement('span');
+            $root.classList.add('cursor-container');
+            const $cursor = document.createElement('span');
+            $cursor.classList.add('cursor');
+            // $user.textContent = 'vivaxy';
+            $root.addEventListener('mouseover', function () {
+              console.log('hover');
+            });
+            $root.appendChild($cursor);
+            return $root;
+          },
+          {
+            type: 'widget',
+            user: '2222',
+            color: 'rgb(255, 100, 255)',
+          },
+        ),
       ]);
     },
     apply(tr, decorationSet) {
@@ -33,6 +53,48 @@ const decorationsPlugin = new Plugin({
     decorations(state) {
       return this.getState(state);
     },
+  },
+  view(editorView) {
+    const mouseMoveHandler = function (e) {
+      const { pos } = editorView.posAtCoords({
+        left: e.clientX - editorView.dom.clientLeft,
+        top: e.clientY - editorView.dom.clientTop,
+      });
+      const decorationSet = decorationsPlugin.getState(editorView.state);
+      const found = decorationSet.find(pos, pos);
+      if (found.length) {
+        console.log('found', found);
+      }
+      const deco = found.sort(function (prev, next) {
+        if (next.spec.type === 'widget') {
+          return 1;
+        } else if (prev.spec.type === 'widget') {
+          return -1;
+        }
+        return 0;
+      });
+      if (deco.length) {
+        console.log('deco', deco);
+        $flag.style.display = 'block';
+        $flag.style.left = `${e.clientX}px`;
+        $flag.style.top = `${e.clientY - 30}px`;
+        $flag.style.background = deco[0].spec.color;
+        $flag.textContent = deco[0].spec.user;
+      } else {
+        $flag.style.display = 'none';
+      }
+    };
+    const $flag = document.createElement('div');
+    $flag.classList.add('flag');
+    $flag.style.position = 'absolute';
+    $flag.style.display = 'none';
+    document.body.appendChild($flag);
+    editorView.dom.addEventListener('mousemove', mouseMoveHandler);
+    return {
+      destroy() {
+        editorView.dom.removeEventListener('mousemove', mouseMoveHandler);
+      },
+    };
   },
 });
 
