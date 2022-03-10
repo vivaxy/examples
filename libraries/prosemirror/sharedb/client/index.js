582dcdfa189b79d5ba02f3d1c9b5992b7c66852c
vivaxy <xyxuye2007@126.com>
2022-03-10 14:32:02 +0800

    feat(prosemirror): add `sharedb` example

--- libraries/prosemirror/sharedb/client/index.js
+++ libraries/prosemirror/sharedb/client/index.js
@@ -5,11 +5,13 @@
 import { schema as basicSchema } from 'prosemirror-schema-basic';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
-import { DOMParser, Schema } from 'prosemirror-model';
+import { DOMParser, Schema, Node } from 'prosemirror-model';
 import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 import { Connection } from 'sharedb/lib/client';
+import jsonDiff from 'json0-ot-diff';
+import { recreateTransform } from '@technik-sde/prosemirror-recreate-transform';
 
 const schema = new Schema({
   nodes: addListNodes(basicSchema.spec.nodes, 'paragraph block*', 'block'),
@@ -49,12 +51,39 @@ doc.subscribe((error) => {
   }
 });
 
-doc.on('op', (op) => {
-  console.log('op', op);
+doc.on('op', (op, local) => {
+  console.log('op', op, local);
+  if (!local) {
+    const { data } = doc.toSnapshot();
+    const pmDoc = Node.fromJSON(schema, data);
+    const transform = recreateTransform(view.state.doc, pmDoc, {
+      complexSteps: true, // Whether step types other than ReplaceStep are allowed.
+      wordDiffs: false, // Whether diffs in text nodes should cover entire words.
+      simplifyDiffs: true, // Whether steps should be merged, where possible
+    });
+    const tr = view.state.tr.setMeta('remote', true);
+    transform.steps.forEach((step) => {
+      tr.step(step);
+    });
+    console.log('steps', tr.steps);
+    view.dispatch(tr);
+  }
 });
 
 const view = new EditorView(document.querySelector('#editor'), {
   state,
+  dispatchTransaction(tr) {
+    if (!tr.getMeta('remote')) {
+      const oldJSON = view.state.doc.toJSON();
+      const newJSON = tr.doc.toJSON();
+      const ops = jsonDiff(oldJSON, newJSON);
+      ops.forEach((op) => {
+        doc.submitOp(op);
+      });
+    }
+    const newState = view.state.apply(tr);
+    view.updateState(newState);
+  },
 });
 
 window.view = view;
