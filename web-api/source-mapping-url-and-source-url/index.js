8e1af4307f4e483ef12902ac79a2e4c0cc26b454
vivaxy <xyxuye2007@126.com>
2022-03-16 10:05:23 +0800

    feat: rename samples to examples

--- web-api/source-mapping-url-and-source-url/index.js
+++ web-api/source-mapping-url-and-source-url/index.js
@@ -3,10 +3,10 @@
  * @author vivaxy
  */
 
-//# sourceURL=samples:///mock.js
+//# sourceURL=examples:///mock.js
 //# sourceMappingURL=http://172.24.236.146:3001/index.js.map
 
 const func = new Function(
-  'console.log(0); //# sourceURL=samples:///console.js',
+  'console.log(0); //# sourceURL=examples:///console.js',
 );
 func();
