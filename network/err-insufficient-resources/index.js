003eb182a64560904806f121abba188fdb2102b5
vivaxy <xyxuye2007@126.com>
2022-05-19 20:23:11 +0800

    feat: err-insufficient-resources

--- network/err-insufficient-resources/index.js
+++ network/err-insufficient-resources/index.js
@@ -1,18 +1,48 @@
 function request(url) {
-  fetch(url, {
+  return fetch(url, {
     mode: 'no-cors',
   });
 }
 
-for (let i = 0; i < 1000; i++) {
-  request(`https://www.baidu.com/`);
-  request(`https://www.qq.com/`);
-  request(`https://www.meituan.com/`);
-  request(`https://www.hao123.com/`);
-  request(`https://www.sina.com.cn/`);
-  request(`https://weibo.com/`);
-  request(`https://www.sohu.com/`);
-  request(`https://www.163.com/`);
-  request(`https://www.126.com/`);
-  console.log(i);
-}
+const HOSTS = [
+  'https://www.baidu.com/',
+  'https://www.qq.com/',
+  'https://www.meituan.com/',
+  'https://www.hao123.com/',
+  'https://www.sina.com.cn/',
+  'https://weibo.com/',
+  'https://www.sohu.com/',
+  'https://www.163.com/',
+  'https://www.126.com/',
+  'https://www.dianping.com/',
+];
+
+document.querySelectorAll('button').forEach((button) => {
+  button.addEventListener('click', async function (e) {
+    const p = [];
+    const { hosts, loops } = e.target.dataset;
+    console.log(`${hosts} Hosts * ${loops} Loops start.`);
+
+    let t = 0;
+    let failed = false;
+    for (let i = 0; i < loops; i++) {
+      for (let h = 0; h < hosts; h++) {
+        const req = async () => {
+          try {
+            await request(HOSTS[h]);
+          } catch (e) {
+            if (!failed) {
+              failed = true;
+              console.error('First fail at ' + t);
+            }
+          } finally {
+            t++;
+          }
+        };
+        p.push(req());
+      }
+    }
+    await Promise.all(p);
+    console.log(`${hosts} Hosts * ${loops} Loops finish.`);
+  });
+});
