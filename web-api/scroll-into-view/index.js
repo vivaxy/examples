a9d3dbc3773e3c5bd4d1b4cf3e9fda08c9f44816
vivaxy <xyxuye2007@126.com>
2022-07-14 10:00:48 +0800

    feat: scroll-into-view

--- web-api/scroll-into-view/index.js
+++ web-api/scroll-into-view/index.js
@@ -5,10 +5,22 @@
 const $target = document.getElementById('target');
 const $container = document.getElementById('container');
 const $scroll = document.getElementById('scroll');
+const $scrollAndScroll = document.getElementById('scroll-and-scroll');
 
-$scroll.addEventListener('click', function () {
+function scroll() {
   $target.scrollIntoView({
     behavior: 'smooth',
     block: 'start',
   });
+}
+
+$scroll.addEventListener('click', scroll);
+
+$scrollAndScroll.addEventListener('click', function () {
+  scroll();
+  function handleScroll() {
+    document.getElementById('mask').scrollLeft = 0;
+    document.removeEventListener('scroll', handleScroll);
+  }
+  document.addEventListener('scroll', handleScroll);
 });
