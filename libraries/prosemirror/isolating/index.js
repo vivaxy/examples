af54b06f7a65ea40e9daf3f46fc12e3f6a1aac47
vivaxy <xyxuye2007@126.com>
2022-09-09 13:29:15 +0800

    feat: prosemirror/isolating

--- libraries/prosemirror/isolating/index.js
+++ libraries/prosemirror/isolating/index.js
@@ -5,6 +5,9 @@
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Schema, DOMParser } from 'prosemirror-model';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap } from 'prosemirror-commands';
+import { history } from 'prosemirror-history';
 
 const doc = {
   content: 'block+',
@@ -13,7 +16,7 @@ const doc = {
 const paragraph = {
   content: 'inline*',
   group: 'block',
-  // isolating: true,
+  isolating: true,
   parseDOM: [{ tag: 'p' }],
   toDOM() {
     return ['p', 0];
@@ -24,13 +27,22 @@ const text = {
   group: 'inline',
 };
 
+const horizontal_rule = {
+  group: 'block',
+  parseDOM: [{ tag: 'hr' }],
+  toDOM() {
+    return ['hr'];
+  },
+};
+
 const schema = new Schema({
-  nodes: { doc, paragraph, text },
+  nodes: { doc, paragraph, text, horizontal_rule },
 });
 
 const view = new EditorView(document.querySelector('#editor'), {
   state: EditorState.create({
     doc: DOMParser.fromSchema(schema).parse(document.querySelector('#content')),
+    plugins: [keymap(baseKeymap), history()],
   }),
 });
 
