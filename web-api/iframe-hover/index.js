0d1d92e365a70fd623d72d2aecd0be95dcc228e3
vivaxy <xyxuye2007@126.com>
2022-10-19 19:59:47 +0800

    feat: iframe hover

--- web-api/iframe-hover/index.js
+++ web-api/iframe-hover/index.js
@@ -6,3 +6,6 @@ const [iframe] = document.getElementsByTagName('iframe');
 iframe.addEventListener('mouseenter', function () {
   console.log('mouseenter');
 });
+iframe.addEventListener('mouseleave', function () {
+  console.log('mouseleave');
+});
