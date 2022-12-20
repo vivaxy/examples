e29a15b8d6fd0de02875af4c11355bbdbaab2bff
vivaxy <xyxuye2007@126.com>
2022-12-20 10:55:22 +0800

    fix: iframe-performance-overhead

--- web-api/iframe-performance-overhead/iframe.js
+++ web-api/iframe-performance-overhead/iframe.js
@@ -1,5 +1,5 @@
 const root = document.getElementById('root');
-for (let i = 0; i < 1e5; i++) {
+for (let i = 0; i < 1e4; i++) {
   const div = document.createElement('div');
   div.innerHTML = i;
   root.appendChild(div);
