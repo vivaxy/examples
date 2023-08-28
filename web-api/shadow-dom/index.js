752e1ff615ec695fda2713377134082494007e28
vivaxy <xyxuye2007@126.com>
2023-08-28 17:13:42 +0800

    feat: update `shadow-dom`

--- web-api/shadow-dom/index.js
+++ web-api/shadow-dom/index.js
@@ -2,15 +2,16 @@
  * @since 2023-05-06
  * @author vivaxy
  */
-const root = document.getElementById('root');
-const shadow = root.attachShadow({ mode: 'closed' });
+const shadowDOMRoot = document.getElementById('shadow-dom-root');
+const shadow = shadowDOMRoot.attachShadow({ mode: 'closed' });
 
 const shadowDOMContent = document.createElement('p');
 shadowDOMContent.classList.add('shadow-dom-content');
+shadowDOMContent.classList.add('styles-out-of-shadow-dom');
 shadowDOMContent.innerHTML = 'ShadowDOM';
 
 const shadowStyle = document.createElement('style');
-shadowStyle.innerHTML = `#root { display: none; } .shadow-dom-content:after { content: 'inside styles' }`;
+shadowStyle.innerHTML = `#root { display: none; } .shadow-dom-content:after { content: ' styles inside shadowDOM' } p { font-size: 24px; }`;
 
 shadow.appendChild(shadowDOMContent);
 shadow.appendChild(shadowStyle);
