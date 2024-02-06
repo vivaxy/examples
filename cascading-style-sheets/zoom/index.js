f908c1c6341eaf8c2543e1937e9a86ccd0f5badc
vivaxy <xyxuye2007@126.com>
2024-02-06 14:41:45 +0800

    feat(cascading-style-sheet/zoom): add logs

--- cascading-style-sheets/zoom/index.js
+++ cascading-style-sheets/zoom/index.js
@@ -4,24 +4,46 @@
  */
 const $test = document.getElementById('test');
 
-function logSize(ele) {
-  const { width, height } = ele.getBoundingClientRect();
-  console.log('boundingClientRect', { width, height });
-  console.log('offsetWidth, offsetHeight', {
-    width: ele.offsetWidth,
-    height: ele.offsetHeight,
-  });
-  console.log('clientWidth, clientHeight', {
-    width: ele.clientWidth,
-    height: ele.clientHeight,
-  });
+/**
+ * @param {string} name
+ * @param {HTMLElement} ele
+ */
+function logSize(name, ele) {
+  const { width, height, left, top } = ele.getBoundingClientRect();
+  console.log(
+    name,
+    'boundingClientRect',
+    { width, height, left, top },
+    'offset',
+    {
+      width: ele.offsetWidth,
+      height: ele.offsetHeight,
+      left: ele.offsetLeft,
+      top: ele.offsetTop,
+    },
+    'client',
+    {
+      width: ele.clientWidth,
+      height: ele.clientHeight,
+      left: ele.clientLeft,
+      top: ele.clientTop,
+    },
+  );
 }
 
 document.addEventListener('click', function (e) {
-  if (e.target.dataset.styleKey) {
+  if (/** @type {HTMLElement} */ (e.target).dataset.styleKey) {
+    const { styleKey, styleValue } = /** @type {HTMLElement} */ (e.target)
+      .dataset;
     $test.removeAttribute('style');
-    $test.style[e.target.dataset.styleKey] = e.target.dataset.styleValue;
-    logSize($test);
-    logSize($test.firstElementChild);
+    $test.style[styleKey] = styleValue;
+    logSize('node', $test);
+    // logSize('child', /** @type {HTMLElement} */ ($test.children[0]));
+  } else if (/** @type {HTMLElement} */ (e.target).textContent === 'Reset') {
+    $test.removeAttribute('style');
+    logSize('node', $test);
+    // logSize('child', /** @type {HTMLElement} */ ($test.children[0]));
   }
 });
+
+logSize('node', $test);
