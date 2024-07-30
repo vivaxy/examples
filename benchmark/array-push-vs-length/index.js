0ee69def1c7add260e5b96106a260d6528561ce6
vivaxy <xyxuye2007@126.com>
2024-07-30 10:02:01 +0800

    feat(array-push-vs-length): update

--- benchmark/array-push-vs-length/index.js
+++ benchmark/array-push-vs-length/index.js
@@ -9,30 +9,49 @@ import {
 } from 'https://unpkg.com/@vivaxy/framework/utils/benchmark.js';
 
 async function main() {
-  // preheat
-  const array = [];
-  for (let i = 0; i < 1e5; i++) {
-    array[array.length] = i;
+  const length = 1e5;
+
+  /**
+   * @param {*[]} array
+   * @param {(arr: *[], i: *) => void} handler
+   */
+  function forEach(array, handler) {
+    for (let i = 0; i < length; i++) {
+      handler(array, i);
+    }
   }
 
-  const arrayPushCost = await run(function () {
-    const array = [];
-    for (let i = 0; i < 1e5; i++) {
-      array.push(i);
-    }
-    return array.length;
+  // preheat
+  const array0 = [];
+  await run(function () {
+    forEach(array0, function (arr, i) {
+      arr[arr.length] = i;
+    });
   });
 
+  const array1 = [];
+  const arrayPushCost = await run(function () {
+    forEach(array1, function (arr, i) {
+      arr.push(i);
+    });
+  });
+
+  const array2 = [];
   const arrayLengthCost = await run(function () {
-    const array = [];
-    for (let i = 0; i < 1e5; i++) {
-      array[array.length] = i;
-    }
-    return array.length;
+    forEach(array2, function (arr, i) {
+      arr[arr.length] = i;
+    });
+  });
+
+  const array3 = Array(length).fill(undefined);
+  const arrayPredefinedLengthCost = await run(function () {
+    forEach(array3, function (arr, i) {
+      arr[arr.length] = i;
+    });
   });
 
   console.log(
-    `array.push() cost ${arrayPushCost}; array[array.length] cost ${arrayLengthCost};`,
+    `array.push() cost ${arrayPushCost}; array[array.length] cost ${arrayLengthCost}; predefined array length, array[array.length] cost ${arrayPredefinedLengthCost};`,
   );
 }
 
