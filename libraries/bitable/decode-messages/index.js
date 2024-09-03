493dda27d639cf31c643c4fa5a27f22a2c268104
vivaxy <xyxuye2007@126.com>
2024-09-03 18:18:21 +0800

    feat(decode-messages): update rows

--- libraries/bitable/decode-messages/index.js
+++ libraries/bitable/decode-messages/index.js
@@ -4,6 +4,10 @@
  */
 import pako from 'https://unpkg.com/pako?module';
 
+/**
+ * @param {string} base64
+ * @return {ArrayBufferLike}
+ */
 function base64ToArrayBuffer(base64) {
   const binaryString = atob(base64);
   const bytes = new Uint8Array(binaryString.length);
@@ -13,23 +17,51 @@ function base64ToArrayBuffer(base64) {
   return bytes.buffer;
 }
 
+/**
+ * @param {string} string
+ * @return {string}
+ */
+function decode(string) {
+  return pako.inflate(base64ToArrayBuffer(string), {
+    to: 'string',
+  });
+}
+
+/**
+ * @param {string} string
+ * @return {string}
+ */
+function encode(string) {
+  // todo
+  const compressed = pako.deflate(string);
+  let binaryString = '';
+  for (let i = 0; i < compressed.length; i++) {
+    binaryString += String.fromCharCode(compressed[i]);
+  }
+  return btoa(binaryString);
+}
+
 document.getElementById('decode').addEventListener('click', () => {
   try {
     const inputValue = /** @type {HTMLTextAreaElement} */ (
       document.getElementById('input')
     ).value;
-    const outputValue = JSON.stringify(
-      JSON.parse(
-        pako.inflate(base64ToArrayBuffer(inputValue), {
-          to: 'string',
-        }),
-      ),
-      null,
-      2,
-    );
     /** @type {HTMLTextAreaElement} */ (
       document.getElementById('output')
-    ).value = outputValue;
+    ).value = JSON.stringify(JSON.parse(decode(inputValue)), null, 2);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document.getElementById('encode').addEventListener('click', function () {
+  try {
+    const inputValue = /** @type {HTMLTextAreaElement} */ (
+      document.getElementById('output')
+    ).value;
+    /** @type {HTMLTextAreaElement} */ (
+      document.getElementById('input')
+    ).value = encode(JSON.stringify(JSON.parse(inputValue)));
   } catch (error) {
     console.error(error);
   }
