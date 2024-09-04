f08846af94c1adf45a76b68e5de0e11a856476f7
vivaxy <xyxuye2007@126.com>
2024-09-04 18:51:26 +0800

    feat(netease-music): init

--- libraries/netease-music/export.js
+++ libraries/netease-music/export.js
@@ -3,14 +3,48 @@
  * @author vivaxy
  */
 import fs from 'fs';
-import path from 'path';
 
 const content = fs.readFileSync('./list.json', 'utf8');
-const list = JSON.parse(content);
-let fileContent = 'Name\tArtist\tComposer\tAlbum\n';
+const list = JSON.parse(content).map(function (item) {
+  return {
+    Name: item.name,
+    Artist: item.ar[0].name,
+    Album: item.al.name,
+  };
+});
+const columns = [
+  'Name',
+  'Artist',
+  // 'Composer',
+  // 'Album',
+  // 'Grouping',
+  // 'Work',
+  // 'Movement Number',
+  // 'Movement Count',
+  // 'Movement Name',
+  // 'Genre',
+  // 'Size',
+  // 'Time',
+  // 'Disc Number',
+  // 'Disc Count',
+  // 'Track Number',
+  // 'Track Count',
+  // 'Year',
+  // 'Date Modified',
+  // 'Date Added',
+  // 'Bit Rate',
+  // 'Sample Rate',
+  // 'Volume Adjustment',
+  // 'Kind',
+  // 'Equalizer',
+];
+let fileContent = columns.join('\t') + '\n';
 
-list.forEach(({ name, ar, al }) => {
-  fileContent += `${name}\t${ar[0].name}\t\t${al.name}\n`;
+list.forEach((object) => {
+  columns.forEach((column) => {
+    fileContent += `${object[column] || ''}\t`;
+  });
+  fileContent += '\n';
 });
 
 fs.writeFileSync('./output.txt', fileContent);
