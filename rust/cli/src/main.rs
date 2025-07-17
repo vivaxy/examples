9494ccad561a80e64956db7626465d37bba89a3d
vivaxy <xyxuye2007@126.com>
2025-07-17 14:09:15 +0800

    feat: rust/cli

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,53 +1,80 @@
 fn main() {
-  let mut inputs: Vec<String> = Vec::new();
-  let args: Vec<String> = std::env::args().collect();
-  let mut ok = args[1].clone() == "create";
+    let mut todos: Vec<String> = Vec::new();
+    todos.push(String::from("learn rust"));
+    todos.push(String::from("work"));
+    todos.push(String::from("play"));
 
-  while ok {
-    let len = inputs.len();
+    let args: Vec<String> = std::env::args().collect();
 
-    if len == 0 {
-      println!("please input todo title:");
-      let mut title = String::new();
+    match args[1].clone().as_str() {
+      "create" => {
+            let mut inputs: Vec<String> = Vec::new();
+            let mut ok = true;
 
-      std::io::stdin().read_line(&mut title);
+            while ok {
+                let len = inputs.len();
 
-      if title.is_empty() {
-        continue;
-      }
+                if len == 0 {
+                    println!("Please input todo title");
 
-      inputs.push(title.trim().to_string());
-    } else if len == 1 {
-      println!("please input todo content");
+                    let mut title = String::new();
 
-      let mut content = String::new();
+                    std::io::stdin()
+                        .read_line(&mut title)
+                        .expect("read line failed");
 
-      std::io::stdin().read_line(&mut content).expect("Failed to read line");
+                    if title.is_empty() {
+                        continue;
+                    }
 
-      if content.is_empty() {
-        continue;
-      }
+                    inputs.push(title.trim().to_string());
+                } else if len == 1 {
+                    println!("Please input todo content");
 
-      inputs.push(content.trim().to_string());
-    } else {
-      println!("title:   [{}]", inputs[0].clone());
-      println!("content: [{}]", inputs[1].clone());
-      println!("Are you sure to create this todo? (y/n)");
+                    let mut content = String::new();
 
-      let mut sure = String::new();
+                    std::io::stdin()
+                        .read_line(&mut content)
+                        .expect("read line failed");
 
-      std::io::stdin().read_line(&mut sure).expect("read line failed");
+                    if content.is_empty() {
+                        continue;
+                    }
 
-      if sure.trim().to_lowercase() != "n" {
-        ok = false;
-      } else {
-        inputs.clear();
-      }
+                    inputs.push(content.trim().to_string());
+                } else {
+                    println!("title:   [{}]", inputs[0].clone());
+                    println!("content: [{}]", inputs[1].clone());
+                    println!("Are you sure to create this todo? (y/n)");
+
+                    let mut sure = String::new();
+
+                    std::io::stdin()
+                        .read_line(&mut sure)
+                        .expect("read line failed");
+
+                    if sure.trim().to_lowercase() != "n" {
+                        ok = false;
+                    } else {
+                        inputs.clear();
+                    }
+                }
+            }
+
+            let title = inputs[0].clone();
+            let content = inputs[1].clone();
+
+            println!("create todo title: {}, content: {}", title, content);
+        }
+
+        "list" => {
+            for todo in &todos {
+                println!("{}", todo);
+            }
+        }
+
+        _ => {
+          println!("unknown command {}", args[1].clone());
+        }
     }
-  }
-
-  let title = inputs[0].clone();
-  let content = inputs[1].clone();
-
-  println!("create todo title: {}, content: {}", title, content);
 }
