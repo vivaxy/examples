ac3220f4100f94536ed262b07cc1e1a413eb532a
vivaxy <xyxuye2007@126.com>
2025-09-18 13:55:38 +0800

    feat: optimize gzip

--- benchmark/gzip/index.js
+++ benchmark/gzip/index.js
@@ -4,11 +4,29 @@
  */
 import pako from 'https://esm.run/pako@2.1.0';
 
-function stringify(string) {
+const methods = [
+  {
+    name: 'toString',
+    encode: toString,
+    decode: toString,
+  },
+  {
+    name: 'gzip',
+    encode: gzipEncode,
+    decode: gzipDecode,
+  },
+  {
+    name: 'gzipBase64',
+    encode: gzipBase64Encode,
+    decode: gzipBase64Decode,
+  },
+];
+
+function toString(string) {
   return string;
 }
 
-function gzip(string) {
+function gzipEncode(string) {
   const compressed = pako.gzip(string);
   let binaryString = '';
   for (let i = 0; i < compressed.length; i++) {
@@ -17,7 +35,15 @@ function gzip(string) {
   return binaryString;
 }
 
-function gzipBase64(string) {
+function gzipDecode(binaryString) {
+  const arr = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    arr[i] = binaryString.charCodeAt(i);
+  }
+  return pako.inflate(arr, { to: 'string' });
+}
+
+function gzipBase64Encode(string) {
   const compressed = pako.gzip(string);
   let binaryString = '';
   for (let i = 0; i < compressed.length; i++) {
@@ -26,17 +52,33 @@ function gzipBase64(string) {
   return btoa(binaryString);
 }
 
-function run(fn) {
+function gzipBase64Decode(base64) {
+  const binaryString = atob(base64);
+  const arr = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    arr[i] = binaryString.charCodeAt(i);
+  }
+  return pako.inflate(arr, { to: 'string' });
+}
+
+function run({ name, encode, decode }) {
   const value = document.getElementById('input').value;
-  console.log('--------', fn.name, '--------');
-  const startTime = performance.now();
-  const result = fn(value);
-  console.log('cost', performance.now() - startTime);
-  console.log('size', new Blob([result]).size);
+  console.log('--------', name, '--------');
+  let startTime = performance.now();
+  const encoded = encode(value);
+  console.log('encode size:', new Blob([encoded]).size, 'bytes');
+  console.log('encode cost:', performance.now() - startTime, 'ms');
+
+  startTime = performance.now();
+  const decoded = decode(encoded);
+  console.log('decode cost:', performance.now() - startTime, 'ms');
+  if (value !== decoded) {
+    throw new Error('value !== decoded');
+  }
 }
 
 document.getElementById('run').addEventListener('click', () => {
-  run(stringify);
-  run(gzip);
-  run(gzipBase64);
+  methods.forEach((method) => {
+    run(method);
+  });
 });
